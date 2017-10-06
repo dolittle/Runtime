ce6b37288c6d61fff9224c03fe7ded420db3bed1
Einar Ingebrigtsen <einar@dolittle.com>
2017-10-06 13:51:26 +0200

    Bringing over Transactions - might end up moving this Fundamentals

---
+++ Source/Transactions/ITransaction.cs
@@ -0,0 +1,24 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+
+namespace doLittle.Runtime.Transactions
+{
+    /// <summary>
+    /// Defines a logical transaction
+    /// </summary>
+    public interface ITransaction : IDisposable
+    {
+        /// <summary>
+        /// Commits the transaction
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Rollback to the state before the transaction started
+        /// </summary>
+        void Rollback();
+    }
+}
