76e6af704c6f7d64bcd0c214e21cb2407d2afc2a
Einar Ingebrigtsen <einar@dolittle.com>
2017-10-06 13:48:30 +0200

    Event Coordination

---
+++ Source/Events.Coordination/ICommittedEventStreamCoordinator.cs
@@ -0,0 +1,17 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace doLittle.Events
+{
+    /// <summary>
+    /// Defines the coordinator that coordinates <see cref="CommittedEventStream">committed event streams</see>
+    /// </summary>
+    public interface ICommittedEventStreamCoordinator
+    {
+        /// <summary>
+        /// Initialize the coordinator
+        /// </summary>
+        void Initialize();
+    }
+}
---
+++ Source/Events.Coordination/IUncommittedEventStreamCoordinator.cs
@@ -0,0 +1,24 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using doLittle.Runtime.Transactions;
+
+namespace doLittle.Events
+{
+    /// <summary>
+    /// Defines a coordinator for dealing with <see cref="UncommittedEventStream"/>
+    /// </summary>
+    public interface IUncommittedEventStreamCoordinator
+    {
+        /// <summary>
+        /// Commit a <see cref="UncommittedEventStream"/>
+        /// </summary>
+        /// <param name="correlationId">
+        /// The <see cref="TransactionCorrelationId"/> related to the <see cref="ITransaction"/> 
+        /// the <see cref="UncommittedEventStream"/> was generated in
+        /// </param>
+        /// <param name="eventStream"><see cref="UncommittedEventStream"/> to commit</param>
+        void Commit(TransactionCorrelationId correlationId, UncommittedEventStream eventStream);
+    }
+}
