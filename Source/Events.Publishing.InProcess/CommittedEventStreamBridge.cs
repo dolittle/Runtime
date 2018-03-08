bfd1bd862690153b96667733856e87475c12d108
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-08 14:39:51 +0100

    Making it explicitly Singleton

--- Source/Events.Publishing.InProcess/CommittedEventStreamBridge.cs
+++ Source/Events.Publishing.InProcess/CommittedEventStreamBridge.cs
@@ -2,11 +2,14 @@
  *  Copyright (c) 2008-2017 doLittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using doLittle.Execution;
+
 namespace doLittle.Runtime.Events.Publishing.InProcess
 {
     /// <summary>
     /// Represents an implementation of <see cref="ICommittedEventStreamBridge"/>
     /// </summary>
+    [Singleton]
     public class CommittedEventStreamBridge : ICommittedEventStreamBridge
     {
         static event CommittedEventStreamReceived _received = (s) => { };
