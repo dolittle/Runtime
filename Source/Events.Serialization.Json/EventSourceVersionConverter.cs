cb1076c80e22980ad2f1491022e936487c60f937
Einar Ingebrigtsen <einar@dolittle.com>
2018-05-24 15:20:39 +0200

    Adding ConvertersProvider

--- Source/Events.Serialization.Json/EventSourceVersionConverter.cs
+++ Source/Events.Serialization.Json/EventSourceVersionConverter.cs
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Dolittle.Events;
 using Newtonsoft.Json;
