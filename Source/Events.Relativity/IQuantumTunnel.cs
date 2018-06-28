e42b4456c968a4a3ccc45aa45c802491bd2648c8
Einar Ingebrigtsen <einar@dolittle.com>
2018-06-28 11:25:31 +0200

    Started hatching out how the event horizon will work

---
+++ Source/Events.Relativity/IQuantumTunnel.cs
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Dolittle.Runtime.Events.Relativity
+{
+    /// <summary>
+    /// Defines the tunnel in which particles known as events pass through
+    /// </summary>
+    public interface IQuantumTunnel
+    {
+
+    }
+}
---
+++ Source/Events.Relativity/ISingularity.cs
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Dolittle.Runtime.Events.Relativity
+{
+    /// <summary>
+    /// Defines the single singularity in which is the destination for particles known as
+    /// events
+    /// </summary>
+    public interface ISingularity
+    {
+
+    }
+}
