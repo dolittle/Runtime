0b3daa44ac0dedb8b6c5c1dc262c32c008265455
Einar Ingebrigtsen <einar@dolittle.com>
2018-08-14 20:32:58 +0200

    Moving away from dependency to ISerializer for JSON

--- Source/Events.Relativity/EventHorizonsConfigurationManager.cs
+++ Source/Events.Relativity/EventHorizonsConfigurationManager.cs
@@ -3,9 +3,13 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System.IO;
+using Dolittle.Collections;
+using Dolittle.Concepts.Serialization.Json;
 using Dolittle.Execution;
 using Dolittle.Logging;
 using Dolittle.Serialization.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Dolittle.Runtime.Events.Relativity
 {
@@ -16,18 +20,19 @@ namespace Dolittle.Runtime.Events.Relativity
     public class EventHorizonsConfigurationManager : IEventHorizonsConfigurationManager
     {
         const string _configurationFile = "event-horizons.json";
+        readonly ILogger _logger;
 
         /// <summary>
         /// Initializes a new instance of <see cref="EventHorizonsConfigurationManager"/>
         /// </summary>
-        /// <param name="serializer"><see cref="ISerializer"/> to use for parsing configuration</param>
         /// <param name="logger"><see cref="ILogger"/> to use for logging</param>
-        public EventHorizonsConfigurationManager(ISerializer serializer, ILogger logger)
+        public EventHorizonsConfigurationManager(ILogger logger)
         {
+            _logger = logger;
             if (File.Exists(_configurationFile))
             {
                 logger.Information("Configuration file found for event horizons, loading it");
-                Current = serializer.FromJson<EventHorizonsConfiguration>(File.ReadAllText(_configurationFile));
+                Current = LoadConfig();
             }
             else
             { 
@@ -38,5 +43,30 @@ namespace Dolittle.Runtime.Events.Relativity
 
         /// <inheritdoc/>
         public EventHorizonsConfiguration Current { get; }
+
+
+        EventHorizonsConfiguration LoadConfig()
+        {
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+            };
+            serializerSettings.Converters.Add(new ConceptConverter());
+            serializerSettings.Converters.Add(new ConceptDictionaryConverter());
+
+            var json = File.ReadAllText(_configurationFile);
+            var current = JsonConvert.DeserializeObject<EventHorizonsConfiguration>(json, serializerSettings);
+            _logger.Information($"EventHorizon will be exposed on TCP with port {current.Port}");
+            _logger.Information($"EventHorizon will be exposed on Unix socket {current.UnixSocket}");
+            current.EventHorizons.ForEach(eventHorizonConfig =>
+            {
+                _logger.Information($"This singularity will be connected to event horizon at '{eventHorizonConfig.Url}' for application '{eventHorizonConfig.Application}' and bounded context '{eventHorizonConfig.BoundedContext}'");
+            });
+
+            return current;
+        }
+
     }
 }
