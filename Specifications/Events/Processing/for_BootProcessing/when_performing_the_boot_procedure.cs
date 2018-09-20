647749c1d0799d31bf31ca1c68d53b2a254082ca
Michael Smith <michael@dolittle.com>
2018-09-20 14:02:32 +0200

    Ensure correct context for Tenant is set before registering event processors (needed for catchup)

--- Specifications/Events/Processing/for_BootProcessing/when_performing_the_boot_procedure.cs
+++ Specifications/Events/Processing/for_BootProcessing/when_performing_the_boot_procedure.cs
@@ -17,6 +17,7 @@ namespace Dolittle.Runtime.Events.Specs.Processing.for_BootProcessing
     using Dolittle.Logging;
     using Dolittle.Runtime.Events.Processing;
     using Dolittle.Runtime.Events.Specs.Processing;
+    using Dolittle.Execution;
 
     [Subject(typeof(BootProcedure), nameof(BootProcedure.Perform))]
     public class when_performing_the_boot_procedure
@@ -27,24 +28,27 @@ namespace Dolittle.Runtime.Events.Specs.Processing.for_BootProcessing
         static Mock<IScopedEventProcessingHub> processing_hub;
         static Exception exception;
         static int number_of_scoped_processors;
+        static int number_of_processors_per_tenant;
         static Mock<IFetchUnprocessedEvents> unprocessed_events_fetcher = new Mock<IFetchUnprocessedEvents>();
         static Mock<IEventProcessorOffsetRepository> offset_repository = new Mock<IEventProcessorOffsetRepository>();
+        static Mock<IExecutionContextManager> execution_context_manager;
 
         Establish context = () =>
         {
+            execution_context_manager = new Mock<IExecutionContextManager>();
             unprocessed_events_fetcher = new Mock<IFetchUnprocessedEvents>();
             offset_repository = new Mock<IEventProcessorOffsetRepository>();
 
             I_know_about_event_processors = get_instances_of_I_know_about_event_processors();
+            I_know_about_event_processors.Object.ForEach(_ => number_of_processors_per_tenant += _.Count()); 
             tenants = get_tenants();
-            I_know_about_event_processors.Object.ForEach(_ => number_of_scoped_processors += _.Count());
-            number_of_scoped_processors *= tenants.Object.All.Count();
-            
+            number_of_scoped_processors = tenants.Object.All.Count() * number_of_processors_per_tenant;
             processing_hub = new Mock<IScopedEventProcessingHub>();
             boot_procedure = new BootProcedure(I_know_about_event_processors.Object,
                                                 tenants.Object,processing_hub.Object, 
                                                 () => offset_repository.Object,
                                                 () => unprocessed_events_fetcher.Object,
+                                                execution_context_manager.Object,
                                                 mocks.a_logger().Object);
         };
 
@@ -52,6 +56,8 @@ namespace Dolittle.Runtime.Events.Specs.Processing.for_BootProcessing
 
         It should_not_have_any_errors = () => exception.ShouldBeNull();
         It should_register_a_scoped_event_processor_for_each_processor_tenant_combination = () => processing_hub.Verify(_ => _.Register(Moq.It.IsAny<ScopedEventProcessor>()),Times.Exactly(number_of_scoped_processors));
+
+        It should_set_the_event_context_for_each_processor_for_each_tenant = () => tenants.Object.All.ForEach(t => execution_context_manager.Verify(_ => _.CurrentFor(t),Times.Exactly(number_of_processors_per_tenant)));
         It should_tell_the_hub_to_begin_processing = () => processing_hub.Verify(_ => _.BeginProcessingEvents(),Times.Once()); 
 
         static Mock<ITenants> get_tenants()
