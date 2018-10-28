f42d220be528950523737f745a34fecb940540b0
Einar Ingebrigtsen <einar@dolittle.com>
2018-10-28 11:01:05 +0100

    Rename - Offset..

--- Source/Events/Relativity/Protobuf/Conversion/TenantOffsetExtensions.cs
+++ Source/Events/Relativity/Protobuf/Conversion/TenantOffsetExtensions.cs
@@ -5,13 +5,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using Dolittle.Tenancy;
+using Google.Protobuf.Collections;
 
 namespace Dolittle.Runtime.Events.Relativity.Protobuf.Conversion
 {
     /// <summary>
     /// Extensions for converting <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset"/> to and from protobuf representations
     /// </summary>
-    public static class TenantOffsetsExtensions
+    public static class TenantOffsetExtensions
     {
         /// <summary>
         /// Convert from <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset"/> to <see cref="TenantOffset"/>
@@ -27,13 +28,38 @@ namespace Dolittle.Runtime.Events.Relativity.Protobuf.Conversion
         }
 
         /// <summary>
+        /// Convert from <see cref="TenantOffset"/> to <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset"/>
+        /// </summary>
+        /// <param name="tenantOffset"><see cref="TenantOffset"/> to convert from</param>
+        /// <returns>Converted <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset"/></returns>
+        public static Dolittle.Runtime.Events.Relativity.TenantOffset ToTenantOffset(this TenantOffset tenantOffset)
+        {
+            return new Dolittle.Runtime.Events.Relativity.TenantOffset(
+                tenantOffset.Tenant.ToConcept<TenantId>(), 
+                tenantOffset.Offset);
+        }
+
+        /// <summary>
+        /// Convert from <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset"/> to collection of <see cref="TenantOffset"/>
+        /// </summary>
+        /// <param name="offsets"><see cref="Dolittle.Runtime.Events.Relativity.TenantOffset">Offsets</see> to convert from</param>
+        /// <returns>Converted <see cref="TenantOffset">offsets</see></returns>
+        public static RepeatedField<TenantOffset> ToProtobuf(this IEnumerable<Dolittle.Runtime.Events.Relativity.TenantOffset> offsets)
+        {
+            var protobuf = new RepeatedField<TenantOffset>();
+            protobuf.Add(offsets.Select(_ => _.ToProtobuf()));
+            return protobuf;
+        }     
+
+
+        /// <summary>
         /// Convert from <see cref="IEnumerable{TenantOffset}"/> to collection of <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset"/>
         /// </summary>
         /// <param name="offsets"><see cref="TenantOffset">Offsets</see> to convert from</param>
         /// <returns>Converted <see cref="Dolittle.Runtime.Events.Relativity.TenantOffset">offsets</see></returns>
         public static IEnumerable<Dolittle.Runtime.Events.Relativity.TenantOffset> ToTenantOffsets(this IEnumerable<TenantOffset> offsets)
         {
-            return offsets.Select(_ => new Dolittle.Runtime.Events.Relativity.TenantOffset(_.Tenant.ToConcept<TenantId>(),_.Offset)).ToList();
+            return offsets.Select(_ => _.ToTenantOffset()).ToArray();
         }     
     }
 }
