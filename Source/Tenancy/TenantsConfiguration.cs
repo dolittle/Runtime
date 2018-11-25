a5379ac7da26200d057f473ebafda90275b04119
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-25 19:47:53 +0100

    Adapting to the new Configuration System

--- Source/Tenancy/TenantsConfiguration.cs
+++ Source/Tenancy/TenantsConfiguration.cs
@@ -2,8 +2,8 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 using System.Collections.Generic;
+using Dolittle.Configuration;
 using Dolittle.Tenancy;
 
 namespace Dolittle.Runtime.Tenancy
@@ -11,11 +11,21 @@ namespace Dolittle.Runtime.Tenancy
     /// <summary>
     /// Represents the configuration for tenants
     /// </summary>
-    public class TenantsConfiguration 
+    [Name("tenants")]
+    public class TenantsConfiguration : IConfigurationObject
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="TenantsConfiguration"/>
+        /// </summary>
+        /// <param name="tenants"><see cref="IDictionary{TKey, TValue}"/> with tenants and their configuration</param>
+        public TenantsConfiguration(IDictionary<TenantId, TenantConfiguration> tenants)
+        {
+            Tenants = tenants;
+        }
+
         /// <summary>
         /// Get the <see cref="TenantConfiguration"/> per <see cref="TenantId"/>
         /// </summary>
-        public Dictionary<TenantId, TenantConfiguration> Tenants { get; set; } = new Dictionary<TenantId, TenantConfiguration>();
+        public IDictionary<TenantId, TenantConfiguration> Tenants { get; }
     }
 }
