e5712dab4991b84508d7e72f938c940d69dbbaca
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-25 19:31:37 +0100

    Changing to taking dependency directly to Application / BC

--- Source/Events/Relativity/BootProcedure.cs
+++ Source/Events/Relativity/BootProcedure.cs
@@ -2,12 +2,13 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using Dolittle.Applications;
 using Dolittle.Applications.Configuration;
 using Dolittle.Bootstrapping;
 using Dolittle.Collections;
 using Dolittle.DependencyInversion;
 using Dolittle.Execution;
-using Dolittle.Resources.Configuration;
+using Dolittle.ResourceTypes.Configuration;
 
 namespace Dolittle.Runtime.Events.Relativity
 {
@@ -20,7 +21,6 @@ namespace Dolittle.Runtime.Events.Relativity
         readonly IEventHorizonsConfigurationManager _configuration;
         readonly IBarrier _barrier;
         readonly IResourceConfiguration _resourceConfiguration;
-        readonly IBoundedContextLoader _boundedContextLoader;
 
         /// <summary>
         /// Initializes a new instance of <see cref="BootProcedure"/>
@@ -28,24 +28,24 @@ namespace Dolittle.Runtime.Events.Relativity
         /// <param name="configuration"><see cref="IEventHorizonsConfigurationManager">Configuration mananger</see></param>
         /// <param name="barrier"><see cref="IBarrier">Barrier</see> to penetrate towards an <see cref="IEventHorizon"/></param>
         /// <param name="resourceConfiguration"></param>
-        /// <param name="boundedContextLoader"></param>
         /// <param name="executionContextManager"></param>
+        /// <param name="application"></param>
+        /// <param name="boundedContext"></param>
         /// <param name="environment">The running environment</param>
         public BootProcedure(
             IEventHorizonsConfigurationManager configuration,
             IBarrier barrier,
             IResourceConfiguration resourceConfiguration,
-            IBoundedContextLoader boundedContextLoader,
             IExecutionContextManager executionContextManager,
+            Application application,
+            BoundedContext boundedContext,
             Environment environment)
         {
             _configuration = configuration;
             _barrier = barrier;
             _resourceConfiguration = resourceConfiguration;
-            _boundedContextLoader = boundedContextLoader;
 
-            var boundedContextConfig = boundedContextLoader.Load();
-            executionContextManager.SetConstants(boundedContextConfig.Application, boundedContextConfig.BoundedContext, environment);
+            executionContextManager.SetConstants(application, boundedContext, environment);
         }
 
         /// <inheritdoc/>
