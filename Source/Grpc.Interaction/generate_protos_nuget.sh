b3ac3fae439a412546667ad706f138c4eb35ee25
Sindre Wilting <sindrewilting@gmail.com>
2019-05-23 19:09:44 +0200

    Follow the folder convention here

--- Source/Grpc.Interaction/generate_protos_nuget.sh
+++ Source/Grpc.Interaction/generate_protos_nuget.sh
@@ -8,4 +8,4 @@ mkdir -p Nuget/Grpc && mkdir -p Nuget/Protobuf
 PROTOC=~/.nuget/packages/grpc.tools/1.8.0/tools/macosx_x64/protoc
 PLUGIN=~/.nuget/packages/grpc.tools/1.8.0/tools/macosx_x64/grpc_csharp_plugin
 
-$PROTOC -I./ -I../../../Protobuf/ --csharp_out ./Nuget/Protobuf  ../../../Protobuf/dolittle/interaction/events.relativity/*.proto ../../../Protobuf/system/*.proto --grpc_out ./Nuget/Grpc --plugin=protoc-gen-grpc=$PLUGIN
+$PROTOC -I./ -I../../Protobuf/ --csharp_out ./Nuget/Protobuf  ../../Protobuf/dolittle/interaction/events.relativity/*.proto ../../Protobuf/system/*.proto --grpc_out ./Nuget/Grpc --plugin=protoc-gen-grpc=$PLUGIN
