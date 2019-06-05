24e5993b77ed56ac800ba728a842cae5458439f7
Sindre Wilting <sindrewilting@gmail.com>
2019-06-05 15:52:42 +0200

    Initial setup for runtime plugin

---
+++ Plugins/runtime/Source/index.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Dolittle. All rights reserved.
+*  Licensed under the MIT License. See LICENSE in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+// import { IPlugin } from '@dolittle/tooling.common.plugins';
+
+import {boilerplates} from '@dolittle/tooling.common.boilerplates';
+console.log('something')
