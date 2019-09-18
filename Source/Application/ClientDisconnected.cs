8427f0d5a0b301644d5828b6fe1adb8b2ecd1919
Einar Ingebrigtsen <einar@dolittle.com>
2019-09-18 09:34:46 +0200

    Cleanup

--- Source/Application/ClientDisconnected.cs
+++ Source/Application/ClientDisconnected.cs
@@ -2,10 +2,6 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-using System;
-using System.Collections.Generic;
-using Grpc.Core;
-
 namespace Dolittle.Runtime.Application
 {
     /// <summary>
