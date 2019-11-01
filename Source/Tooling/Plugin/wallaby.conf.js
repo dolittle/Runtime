fd211591757fc086aed15044ab41b952ba109bb9
Sindre Wilting <sindrewilting@gmail.com>
2019-11-01 13:41:56 +0100

    Adapt to use new build

---
+++ Source/Tooling/Plugin/wallaby.conf.js
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+const build = require('@dolittle/typescript.build.node');
+
+module.exports = build.wallaby();
