703ec0af371f30a40dac951262965661615815e8
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-14 09:21:16 +0100

    Static code analysis fixes

--- Source/Commands.Security/ICommandSecurityManager.cs
+++ Source/Commands.Security/ICommandSecurityManager.cs
@@ -1,22 +1,20 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Dolittle.Security;
-using Dolittle.Runtime.Commands;
 
 namespace Dolittle.Runtime.Commands.Security
 {
     /// <summary>
-    /// Defines a manager for dealing with security for <see cref="CommandRequest">commands</see>
+    /// Defines a manager for dealing with security for <see cref="CommandRequest">commands</see>.
     /// </summary>
     public interface ICommandSecurityManager
     {
         /// <summary>
-        /// Authorizes a <see cref="CommandRequest"/> 
+        /// Authorizes a <see cref="CommandRequest"/>.
         /// </summary>
-        /// <param name="command"><see cref="CommandRequest"/> to ask for</param>
-        /// <returns><see cref="AuthorizationResult"/> that details how the <see cref="CommandRequest"/> was authorized</returns>
+        /// <param name="command"><see cref="CommandRequest"/> to ask for.</param>
+        /// <returns><see cref="AuthorizationResult"/> that details how the <see cref="CommandRequest"/> was authorized.</returns>
         AuthorizationResult Authorize(CommandRequest command);
     }
 }
