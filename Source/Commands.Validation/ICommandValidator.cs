d6d196b136ca235a754a345428332e20f5271026
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-14 09:23:03 +0100

    Static code analysis fixes

--- Source/Commands.Validation/ICommandValidator.cs
+++ Source/Commands.Validation/ICommandValidator.cs
@@ -1,19 +1,18 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace Dolittle.Runtime.Commands.Validation
 {
     /// <summary>
-    /// Validates that a command is valid and conforms to simple business rules
+    /// Validates that a command is valid and conforms to simple business rules.
     /// </summary>
     public interface ICommandValidator
     {
         /// <summary>
-        /// Validate the command
+        /// Validate the command.
         /// </summary>
-        /// <param name="command">Instance to be validated</param>
-        /// <returns>Validation results for a <see cref="CommandRequest">Command</see></returns>
+        /// <param name="command">Instance to be validated.</param>
+        /// <returns>Validation results for a <see cref="CommandRequest">Command</see>.</returns>
         CommandValidationResult Validate(CommandRequest command);
     }
 }
--- Source/Commands.Validation/ICommandValidators.cs
+++ Source/Commands.Validation/ICommandValidators.cs
@@ -1,19 +1,18 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace Dolittle.Runtime.Commands.Validation
 {
     /// <summary>
-    /// Defines the validators for validating a <see cref="CommandRequest"/>
+    /// Defines the validators for validating a <see cref="CommandRequest"/>.
     /// </summary>
     public interface ICommandValidators
     {
         /// <summary>
-        /// Validate the command
+        /// Validate the command.
         /// </summary>
-        /// <param name="command">Instance to be validated</param>
-        /// <returns>Validation results for a <see cref="CommandRequest">Command</see></returns>
+        /// <param name="command">Instance to be validated.</param>
+        /// <returns>Validation results for a <see cref="CommandRequest">Command</see>.</returns>
         CommandValidationResult Validate(CommandRequest command);
     }
 }
