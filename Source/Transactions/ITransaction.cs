84c523f0b0b9f73f239f79dcb98bba5054f2abf4
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-14 09:23:58 +0100

    Static code analysis fixes

--- Source/Transactions/ITransaction.cs
+++ Source/Transactions/ITransaction.cs
@@ -1,23 +1,22 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 
 namespace Dolittle.Runtime.Transactions
 {
     /// <summary>
-    /// Defines a logical transaction
+    /// Defines a logical transaction.
     /// </summary>
     public interface ITransaction : IDisposable
     {
         /// <summary>
-        /// Commits the transaction
+        /// Commits the transaction.
         /// </summary>
         void Commit();
 
         /// <summary>
-        /// Rollback to the state before the transaction started
+        /// Rollback to the state before the transaction started.
         /// </summary>
         void Rollback();
     }
