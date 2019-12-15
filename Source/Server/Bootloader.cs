f775e09944c69a76e5a4e470d8e7a8cb1e1ea398
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-15 11:50:16 +0100

    Static code analysis fixes

--- Source/Server/Bootloader.cs
+++ Source/Server/Bootloader.cs
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Threading.Tasks;
 using Dolittle.Booting;
 using Microsoft.Extensions.Hosting;
@@ -10,20 +9,18 @@ using Microsoft.Extensions.Logging;
 namespace Dolittle.Runtime.Server
 {
     /// <summary>
-    /// Represents a bootloader for booting a server
+    /// Represents a bootloader for booting a server.
     /// </summary>
-    public class Bootloader
+    public static class Bootloader
     {
         /// <summary>
-        /// Start booting the server
+        /// Start booting the server.
         /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task Start()
         {
             var hostBuilder = new HostBuilder();
-            hostBuilder.ConfigureLogging(_ =>
-            {
-                _.AddConsole();
-            });
+            hostBuilder.ConfigureLogging(_ => _.AddConsole());
             hostBuilder.UseEnvironment("Development");
             var host = hostBuilder.Build();
             var loggerFactory = host.Services.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
@@ -34,7 +31,7 @@ namespace Dolittle.Runtime.Server
                 _.Development();
             }).Start();
 
-            await host.RunAsync();
+            await host.RunAsync().ConfigureAwait(false);
         }
     }
 }
