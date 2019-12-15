d5f7bf296f6e6d43e44c723ec91c20903bc0ea45
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-15 10:00:07 +0100

    Static code analysis fixes

--- Source/Microservices/MicroserviceServiceType.cs
+++ Source/Microservices/MicroserviceServiceType.cs
@@ -1,17 +1,16 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using Dolittle.Services;
 
 namespace Dolittle.Runtime.Microservices
 {
     /// <summary>
-    /// Represents a <see cref="IRepresentServiceType">host type</see> that is for interaction communication
+    /// Represents a <see cref="IRepresentServiceType">host type</see> that is for interaction communication.
     /// </summary>
     /// <remarks>
-    /// Interaction is considered the channel in which a representation of processes interacting
+    /// Interaction is considered the channel in which a representation of processes interacting.
     /// </remarks>
     public class MicroserviceServiceType : IRepresentServiceType
     {
