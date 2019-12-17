c8f5d6f2d12d1d17c9898a21d16a55f5e12bf404
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-17 18:44:24 +0100

    Static code analysis fixes

--- Specifications/Heads/for_HeadServiceDefinition/when_creating_instance_with_type_not_inheriting_client_base.cs
+++ Specifications/Heads/for_HeadServiceDefinition/when_creating_instance_with_type_not_inheriting_client_base.cs
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using Machine.Specifications;
 
