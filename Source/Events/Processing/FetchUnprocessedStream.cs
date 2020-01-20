a29899e607a9813dd2e2c62edeb6590409408f14
Sindre Wilting <sindrewilting@gmail.com>
2020-01-20 20:44:44 +0100

    IFetchUnprocessedStream

---
+++ Source/Events/Processing/FetchUnprocessedStream.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using Dolittle.DependencyInversion;
+using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Events.Streams;
+
+namespace Dolittle.Runtime.Events.Processing
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IFetchUnprocessedStream" />.
+    /// </summary>
+    public class FetchUnprocessedStream : IFetchUnprocessedStream
+    {
+        readonly FactoryFor<IEventStore> _getEventStore;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FetchUnprocessedStream"/> class.
+        /// </summary>
+        /// <param name="getEventStore"><see cref="FactoryFor{IEventStore}"/> for <see cref="IEventStore"/>.</param>
+        public FetchUnprocessedStream(FactoryFor<IEventStore> getEventStore)
+        {
+            _getEventStore = getEventStore;
+        }
+
+        /// <inheritdoc />
+        public IObservable<CommittedEventEnvelope> GetUnprocessedStream(StreamPosition streamPostion)
+        {
+            using (var eventStore = _getEventStore())
+            {
+                // var eventStream = eventStore.FetchAllEventsOfTypeAfter(eventType, committedEventVersion.Major);
+                var stream = new List<CommittedEventEnvelope>().ToObservable();
+                return stream;
+            }
+        }
+    }
+}
---
+++ Source/Events/Processing/IFetchUnprocessedStream.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Events.Streams;
+
+namespace Dolittle.Runtime.Events.Processing
+{
+    /// <summary>
+    /// Defines an interface for getting an unprocessed stream for an event processor.
+    /// </summary>
+    public interface IFetchUnprocessedStream
+    {
+        /// <summary>
+        /// Get unprocessed stream.
+        /// </summary>
+        /// <param name="streamPosition"><see cref="StreamPosition">the position in the stream</see>.</param>
+        /// <returns>An unprocessed stream.</returns>
+        IObservable<CommittedEventEnvelope> GetUnprocessedStream(StreamPosition streamPosition);
+    }
+}
