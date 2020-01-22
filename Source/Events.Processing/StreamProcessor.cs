e40afa54e92da1739326491e34045f1178a97a4c
Sindre Wilting <sindrewilting@gmail.com>
2020-01-22 13:02:02 +0100

    Work on Stream Processor

--- Source/Events.Processing/StreamProcessor.cs
+++ Source/Events.Processing/StreamProcessor.cs
@@ -2,150 +2,247 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Reactive.Linq;
-using Dolittle.Collections;
+using System.Threading;
+using System.Threading.Tasks;
 using Dolittle.DependencyInversion;
 using Dolittle.Logging;
-using Dolittle.Runtime.Events.Processing;
 using Dolittle.Runtime.Events.Store;
 using Dolittle.Tenancy;
 
-namespace Dolittle.Runtime.Events.Streams.Processing
+namespace Dolittle.Runtime.Events.Processing
 {
     /// <summary>
     /// Processes an individual <see cref="CommittedEventEnvelope" /> for the correct <see cref="TenantId" />.
     /// </summary>
     public class StreamProcessor
     {
-        readonly object lockObject = new object();
+        const int TimeToWait = 1000;
         readonly TenantId _tenant;
-        readonly IEventProcessor _processor;
+        readonly IEventProcessorNew _processor;
         readonly ILogger _logger;
-        readonly FactoryFor<IFetchUnprocessedStream> _getUnprocessedStream;
-        readonly FactoryFor<IStreamProcessorStateAndPositionRepository> _getStateAndPositionRepository;
+        readonly FactoryFor<IFetchNextEvent> _getNextEventFetcher;
+        readonly FactoryFor<IStreamProcessorStateRepository> _getStreamProcessorStateRepository;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="StreamProcessor"/> class.
         /// </summary>
         /// <param name="tenant">The <see cref="TenantId" /> that this processor is scoped to.</param>
-        /// <param name="processor">An <see cref="IEventProcessor" /> to process the event.</param>
-        /// <param name="getStateAndPositionRepository">A factory function to return a correctly scoped instance of <see cref="IStreamProcessorStateAndPositionRepository" />.</param>
-        /// <param name="getUnprocessedStream">A factory function to return a correctly scoped instance of <see cref="IFetchUnprocessedStream" />.</param>
+        /// <param name="sourceStreamId">The <see cref="StreamId" /> of the source stream.</param>
+        /// <param name="processor">An <see cref="IEventProcessorNew" /> to process the event.</param>
+        /// <param name="getStreamProcessorStateRepository">A factory function to return a correctly scoped instance of <see cref="IStreamProcessorStateRepository" />.</param>
+        /// <param name="getNextEventFetcher">A factory function to return a correctly scoped instance of <see cref="IFetchNextEvent" />.</param>
         /// <param name="logger">An <see cref="ILogger" /> to log messages.</param>
         public StreamProcessor(
             TenantId tenant,
-            IEventProcessor processor,
-            FactoryFor<IStreamProcessorStateAndPositionRepository> getStateAndPositionRepository,
-            FactoryFor<IFetchUnprocessedStream> getUnprocessedStream,
+            StreamId sourceStreamId,
+            IEventProcessorNew processor,
+            FactoryFor<IStreamProcessorStateRepository> getStreamProcessorStateRepository,
+            FactoryFor<IFetchNextEvent> getNextEventFetcher,
             ILogger logger)
         {
             _tenant = tenant;
             _processor = processor;
             _logger = logger;
-            _getUnprocessedStream = getUnprocessedStream;
-            _getStateAndPositionRepository = getStateAndPositionRepository;
+            _getNextEventFetcher = getNextEventFetcher;
+            _getStreamProcessorStateRepository = getStreamProcessorStateRepository;
+            Key = new StreamProcessorKey(_processor.Identifier, sourceStreamId);
+            LogMessageBeginning = $"Stream Processor for tenant '{tenant.Value}', event processor '{Key.EventProcessorId.Value}' with source stream '{Key.SourceStreamId.Value}'";
         }
-
-        /// <summary>
-        /// Gets the <see cref="StreamId">stream id</see>.
-        /// </summary>
-        public StreamId StreamId { get; }
 
         /// <summary>
         /// Gets the unique identifer for the <see cref="IEventProcessor" />.
         /// </summary>
-        public EventProcessorId ProcessorId => _processor?.Identifier;
+        public StreamProcessorKey Key { get; }
 
         /// <summary>
-        /// Gets a value indicating whether the processor has caught up with committed events.
+        /// Gets the current <see cref="StreamProcessorState" />.
         /// </summary>
-        public bool HasCaughtUp { get; private set; }
+        public StreamProcessorState CurrentState { get; private set; }
+
+        string LogMessageBeginning { get; }
+
+        bool RetryClockIsTicking { get; } = false;
 
         /// <summary>
-        /// Gets the current <see cref="StreamProcessorStateAndPosition" />.
+        /// Starts up the processing of the stream. It will continiously process and wait for events to process until it is stopped.
         /// </summary>
-        public StreamProcessorStateAndPosition CurrentStateAndPosition { get; private set; } = StreamProcessorStateAndPosition.New;
-
-        /// <summary>
-        /// Instructs the Processor to Catch up by processing events that have been committed since the last processed event.
-        /// </summary>
-        public virtual void CatchUp()
+        public async void Start()
         {
-            using (var repository = _getStateAndPositionRepository())
-            {
-                CurrentStateAndPosition = repository.Get(ProcessorId);
-            }
-
-            var stream = _getUnprocessedStream().GetUnprocessedStream(CurrentStateAndPosition.Position);
-
-            HasCaughtUp = true;
-        }
-
-        /// <summary>
-        /// Processes a single <see cref="CommittedEventEnvelope" />.
-        /// </summary>
-        /// <param name="envelope">The <see cref="CommittedEventEnvelope" />to process.</param>
-        public virtual void Process(CommittedEventEnvelope envelope)
-        {
-            lock (lockObject)
-            {
-                Console.WriteLine($"Event Processor: {_processor.Identifier.Value} Checking if can process: {envelope.Version.Major}.{envelope.Version.Minor}.{envelope.Version.Revision} - {envelope.Metadata.Artifact.Id} : {CanProcess(envelope.Version)}");
-                if (CanProcess(envelope.Version))
-                {
-                    Console.WriteLine($"Event Processor: {_processor.Identifier.Value} Processing: {envelope.Version.Major}.{envelope.Version.Minor}.{envelope.Version.Revision} - {envelope.Metadata.Artifact.Id}");
-                    ProcessEvent(envelope);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Indicates whether the processor can process the <see cref="CommittedEventVersion" /> provided.
-        /// </summary>
-        /// <param name="version"><see cref="CommittedEventVersion"/> to check for.</param>
-        /// <returns>True if the processor has caught up and the version is greater than the last processed version, otherwise false.</returns>
-        public bool CanProcess(CommittedEventVersion version)
-        {
-            return HasCaughtUp && version > LastVersionProcessed;
-        }
-
-        void ProcessEvent(CommittedEventEnvelope envelope)
-        {
-            lock (lockObject)
-            {
-                _logger.Debug($"Processing {envelope.Version} of {Key.Event.Id}");
-                var previousVersion = LastVersionProcessed;
-                try
-                {
-                    SetCurrentVersion(envelope.Version);
-                    _processor.Process(envelope);
-                    _logger.Debug($"Processed {envelope.Version} of {Key.Event.Id}");
-                }
-                catch (Exception ex)
-                {
-                    _logger.Error($"Error Processing {envelope.Version} of {Key.Event.Id}.  Resetting to previous version. {ex.Message}");
-                    SetCurrentVersion(previousVersion);
-                    throw;
-                }
-            }
-
-            _logger.Debug($"Processing {envelope.Version} of {Key.Event.Id}");
-        }
-
-        void SetCurrentVersion(CommittedEventVersion committedEventVersion)
-        {
-            // the provider should accept the version and is responsible for transient errors and persisting it eventually
             try
             {
-                LastVersionProcessed = committedEventVersion;
-                using (var repository = _getStateAndPositionRepository())
+                _logger.Debug($"{LogMessageBeginning} is starting up.");
+                using (var repository = _getStreamProcessorStateRepository())
                 {
-                    repository.Set(ProcessorId, LastVersionProcessed);
+                    CurrentState = repository.Get(Key) ?? StreamProcessorState.New;
+                }
+
+                if (IsWaiting()) SetState(StreamProcessingState.Processing);
+                while (!IsStopping())
+                {
+                    if (IsProcessing()) await EnterProcessing().ConfigureAwait(false);
+                    else if (IsRetrying()) await EnterRetrying().ConfigureAwait(false);
                 }
             }
             catch (Exception ex)
             {
-                _logger.Error($"Error setting offset for '{ProcessorId}' : '{committedEventVersion}' - {ex}");
+                _logger.Debug($"{LogMessageBeginning}: Error while processing - {ex}");
+            }
+
+            _logger.Debug($"{LogMessageBeginning} has stopped processing.");
+        }
+
+        async Task EnterProcessing()
+        {
+            try
+            {
+                _logger.Debug($"{LogMessageBeginning} is starting to process.");
+                while (IsProcessing())
+                {
+                    var @event = await FetchNextEvent().ConfigureAwait(false);
+                    if (@event == null)
+                    {
+                        _logger.Debug($"{LogMessageBeginning} has no event to process.");
+                        Wait();
+                    }
+                    else
+                    {
+                        await ProcessEvent(@event).ConfigureAwait(false);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"{LogMessageBeginning}: Error while processing - {ex}");
+                throw;
             }
         }
+
+        async Task EnterRetrying()
+        {
+            try
+            {
+                // TODO: Start clock
+                // TODO: Store retry_timeout in state
+                _logger.Warning($"{LogMessageBeginning} processing failed. Entering retrying state.");
+                var @event = await FetchNextEvent().ConfigureAwait(false);
+                while (IsRetrying())
+                {
+                    _logger.Debug($"{LogMessageBeginning} is waiting to retry");
+                    Thread.Sleep(TimeToWait);
+                    _logger.Debug($"{LogMessageBeginning} is trying to process event with artifact id '{@event.Metadata.Artifact.Id.Value}' again.");
+                    await ProcessEvent(@event).ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"{LogMessageBeginning}: Error while retrying - {ex}");
+                throw;
+            }
+        }
+
+        async Task<CommittedEventEnvelope> FetchNextEvent()
+        {
+            try
+            {
+                _logger.Debug($"{LogMessageBeginning} is fetching next event.");
+                var nextEventFetcher = _getNextEventFetcher();
+                return await nextEventFetcher.FetchNextEvent(Key.SourceStreamId, CurrentState.Position).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"{LogMessageBeginning}: Error while fetching next event - {ex}");
+                throw;
+            }
+        }
+
+        async Task ProcessEvent(CommittedEventEnvelope @event)
+        {
+            try
+            {
+                _logger.Debug($"{LogMessageBeginning} is processing event with artifact id '{@event.Metadata.Artifact.Id}'");
+                SetState(StreamProcessingState.Processing);
+                var processingResult = await _processor.Process(@event).ConfigureAwait(false);
+
+                HandleProcessingResult(@event, processingResult);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"{LogMessageBeginning}: Error processing event with artifact id '{@event.Metadata.Artifact.Id.Value} - {ex.Message}");
+                throw;
+            }
+        }
+
+        void HandleProcessingResult(CommittedEventEnvelope @event, IProcessingResult processingResult)
+        {
+            switch (processingResult.Value)
+            {
+                case ProcessingResultValue.Succeeded:
+                    _logger.Debug($"{LogMessageBeginning} processed event with artifact id '{@event.Metadata.Artifact.Id}'");
+                    IncrementPosition();
+                    break;
+                case ProcessingResultValue.Retry:
+                    SetState(StreamProcessingState.Retrying);
+                    break;
+                case ProcessingResultValue.Failed:
+                    Stop();
+                    break;
+            }
+        }
+
+        void Wait(int milliseconds = TimeToWait)
+        {
+            _logger.Debug($"{LogMessageBeginning} is waiting...");
+            SetState(StreamProcessingState.Waiting);
+            Thread.Sleep(milliseconds);
+            SetState(StreamProcessingState.Processing);
+        }
+
+        void Stop()
+        {
+            _logger.Error($"{LogMessageBeginning} is stopping");
+            SetState(StreamProcessingState.Stopping);
+        }
+
+        void IncrementPosition()
+        {
+            _logger.Debug($"{LogMessageBeginning} is incrementing its position in the source stream '{Key.SourceStreamId.Value}'");
+            SetState(StreamProcessingState.Processing, CurrentState.Position.Increment());
+        }
+
+        void SetState(StreamProcessingState state) => SetState(state, CurrentState.Position);
+
+        void SetState(StreamProcessingState state, StreamPosition position)
+        {
+            if (IsInState(state, position)) return;
+            try
+            {
+                using (var repository = _getStreamProcessorStateRepository())
+                {
+                    _logger.Debug($"{LogMessageBeginning} is setting new state to {CurrentState}");
+                    repository.Set(Key, CurrentState);
+                    CurrentState = new StreamProcessorState(state, position);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"{LogMessageBeginning}: Error setting new state - {ex}");
+
+                // This is a weird scenario where the state cannot be persisted in the event store.
+                CurrentState = new StreamProcessorState(StreamProcessingState.Stopping, CurrentState.Position);
+                throw;
+            }
+        }
+
+        bool IsProcessing() => IsInProcessingState(StreamProcessingState.Processing);
+
+        bool IsWaiting() => IsInProcessingState(StreamProcessingState.Waiting);
+
+        bool IsRetrying() => IsInProcessingState(StreamProcessingState.Retrying);
+
+        bool IsStopping() => IsInProcessingState(StreamProcessingState.Stopping);
+
+        bool IsInProcessingState(StreamProcessingState state) => CurrentState.State == state;
+
+        bool IsInState(StreamProcessingState state, StreamPosition position) => state == CurrentState.State && position == CurrentState.Position;
     }
 }
