9c0d77925bd76be81b700b0d7ad89876ff6f84a8
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-01-25 12:34:47 +0100

    Specifications for Events/UncommittedAggregateEvents

---
+++ Specifications/Events/for_UncommittedAggregateEvents/when_creating.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Machine.Specifications;
+
+namespace Dolittle.Runtime.Events.Specs.for_UncommittedAggregateEvents
+{
+    public class when_creating : given.an_aggregate_and_two_events
+    {
+        static UncommittedAggregateEvents events;
+
+        Because of = () => events = new UncommittedAggregateEvents(event_source_id, aggregate_root_type, aggregate_root_version);
+
+        It should_not_have_events = () => events.HasEvents.ShouldBeFalse();
+        It should_have_a_count_of_zero = () => events.Count.ShouldEqual(0);
+    }
+}
