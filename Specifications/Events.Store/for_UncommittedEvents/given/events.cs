78c1fb4b9f4ecbbbcbbc7474792236443e78086b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-01-25 17:03:29 +0100

    Specifications for uncommitted events

---
+++ Specifications/Events.Store/for_UncommittedEvents/given/events.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Artifacts;
+using Machine.Specifications;
+
+namespace Dolittle.Runtime.Events.Store.Specs.for_UncommittedEvents.given
+{
+    public abstract class events
+    {
+        public static Artifact event_a_artifact = new Artifact(Guid.Parse("d26cc060-9153-4988-8f07-3cf67f58bf47"), ArtifactGeneration.First);
+        public static Artifact event_b_artifact = new Artifact(Guid.Parse("cc657c0a-2c81-4338-85a8-507f05d4fc0e"), ArtifactGeneration.First);
+
+        public static UncommittedEvent event_one;
+        public static UncommittedEvent event_two;
+        public static UncommittedEvent event_three;
+
+        Establish context = () =>
+        {
+            event_one = new UncommittedEvent(event_a_artifact, "one");
+            event_two = new UncommittedEvent(event_a_artifact, "two");
+            event_three = new UncommittedEvent(event_b_artifact, "three");
+        };
+    }
+}
