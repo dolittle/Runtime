67dcbdce2bd7fbeb55ba1abd29f28e3bcb4fb05a
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-01-27 08:02:11 +0100

    Bring back the EventStore resource type

--- Source/Events.Store/EventStoreResourceType.cs
+++ Source/Events.Store/EventStoreResourceType.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-/*
 using System;
 using System.Collections.Generic;
 using Dolittle.ResourceTypes;
-using Dolittle.Runtime.Events.Processing;
 using Dolittle.Runtime.Events.Store;
 
 namespace Dolittle.Events
@@ -19,7 +17,6 @@ namespace Dolittle.Events
         public ResourceType Name => "eventStore";
 
         /// <inheritdoc/>
-        public IEnumerable<Type> Services { get; } = new[] { typeof(IEventStore), typeof(IEventProcessorOffsetRepository) };
+        public IEnumerable<Type> Services { get; } = new[] { typeof(IEventStore) };
     }
 }
-*/
