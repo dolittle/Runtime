68115c8ba8cc42b389e87f1ce2ca8102e77ea8fc
Einar Ingebrigtsen <einar@dolittle.com>
2020-02-12 17:35:25 +0100

    EventHandler + EventProcessor up and running

--- Source/Events.Processing/EventProcessor.cs
+++ Source/Events.Processing/EventProcessor.cs
@@ -1,8 +1,14 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+extern alias contracts;
+
 using System.Threading.Tasks;
+using contracts::Dolittle.Runtime.Events.Processing;
+using Dolittle.Execution;
 using Dolittle.Logging;
+using Dolittle.Protobuf;
+using Dolittle.Services;
 
 namespace Dolittle.Runtime.Events.Processing
 {
@@ -11,6 +17,8 @@ namespace Dolittle.Runtime.Events.Processing
     /// </summary>
     public class EventProcessor : IEventProcessor
     {
+        readonly IReverseCallDispatcher<EventHandlerClientToRuntimeResponse, EventHandlerRuntimeToClientRequest> _callDispatcher;
+        readonly IExecutionContextManager _executionContextManager;
         readonly ILogger _logger;
         readonly string _logMessagePrefix;
 
@@ -18,14 +26,20 @@ namespace Dolittle.Runtime.Events.Processing
         /// Initializes a new instance of the <see cref="EventProcessor"/> class.
         /// </summary>
         /// <param name="id">The <see cref="EventProcessorId" />.</param>
+        /// <param name="callDispatcher"><see cref="IReverseCallDispatcher{TResponse, TRequest}"/> for server requests.</param>
+        /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for current <see cref="Execution.ExecutionContext"/>.</param>
         /// <param name="logger">The <see cref="ILogger" />.</param>
         public EventProcessor(
             EventProcessorId id,
+            IReverseCallDispatcher<EventHandlerClientToRuntimeResponse, EventHandlerRuntimeToClientRequest> callDispatcher,
+            IExecutionContextManager executionContextManager,
             ILogger logger)
         {
+            _callDispatcher = callDispatcher;
+            _executionContextManager = executionContextManager;
             Identifier = id;
             _logger = logger;
-            _logMessagePrefix = $"Remote Event Processor '{Identifier}'";
+            _logMessagePrefix = $"Event Processor '{Identifier}'";
         }
 
         /// <inheritdoc />
@@ -36,7 +50,14 @@ namespace Dolittle.Runtime.Events.Processing
         {
             _logger.Debug($"{_logMessagePrefix} is processing event '{@event.Type.Id.Value}' for partition '{partitionId.Value}'");
 
-            await Task.CompletedTask.ConfigureAwait(false);
+            var message = new EventHandlerRuntimeToClientRequest
+            {
+                Event = @event.ToProtobuf(),
+                ExecutionContext = _executionContextManager.Current.ToByteString()
+            };
+
+            await _callDispatcher.Call(message, _ => { }).ConfigureAwait(false);
+
             return new SucceededProcessingResult();
         }
     }
---
+++ Source/Events.Store/EventStoreService.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+extern alias contracts;
+
+using System.Threading.Tasks;
+using contracts::Dolittle.Runtime.Events;
+using Dolittle.Logging;
+using Grpc.Core;
+using static contracts::Dolittle.Runtime.Events.EventStore;
+using grpc = contracts::Dolittle.Runtime.Events;
+
+namespace Dolittle.Runtime.Events.Store
+{
+    /// <summary>
+    /// Represents the implementation of.
+    /// </summary>
+    public class EventStoreService : EventStoreBase
+    {
+        readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventStoreService"/> class.
+        /// </summary>
+        /// <param name="logger"><see cref="ILogger"/> for logging.</param>
+        public EventStoreService(
+            ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public override async Task<EventCommitResponse> Commit(grpc.UncommittedEvents request, ServerCallContext context)
+        {
+            _logger.Debug($"Events received : {request.Events.Count}");
+            return await Task.FromResult(new EventCommitResponse()).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        public override async Task<EventCommitResponse> CommitForAggregate(grpc.UncommittedAggregateEvents request, ServerCallContext context)
+        {
+            _logger.Debug("Events for Aggregate received");
+            return await Task.FromResult(new EventCommitResponse()).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        public override async Task<grpc.CommittedAggregateEvents> FetchForAggregate(Aggregate request, ServerCallContext context)
+        {
+            _logger.Debug("Fetch for Aggregate");
+            return await Task.FromResult(new grpc.CommittedAggregateEvents()).ConfigureAwait(false);
+        }
+    }
+}
