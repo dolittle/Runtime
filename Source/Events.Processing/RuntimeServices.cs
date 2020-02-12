4bc9e3b541435f9d9dfcc0bba73530b75a2e923f
Einar Ingebrigtsen <einar@dolittle.com>
2020-02-12 17:36:15 +0100

    Adding EventHandlersService as hosted service

--- Source/Events.Processing/RuntimeServices.cs
+++ Source/Events.Processing/RuntimeServices.cs
@@ -17,14 +17,17 @@ namespace Dolittle.Runtime.Events.Processing
     public class RuntimeServices : ICanBindRuntimeServices
     {
         readonly FiltersService _filtersService;
+        readonly EventHandlersService _eventHandlers;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RuntimeServices"/> class.
         /// </summary>
         /// <param name="filtersService">The <see cref="FiltersService"/>.</param>
-        public RuntimeServices(FiltersService filtersService)
+        /// <param name="eventHandlers">The <see cref="EventHandlersService"/>.</param>
+        public RuntimeServices(FiltersService filtersService, EventHandlersService eventHandlers)
         {
             _filtersService = filtersService;
+            _eventHandlers = eventHandlers;
         }
 
         /// <inheritdoc/>
@@ -35,7 +38,8 @@ namespace Dolittle.Runtime.Events.Processing
         {
             return new Service[]
             {
-                new Service(_filtersService, grpc.Filters.BindService(_filtersService), grpc.Filters.Descriptor)
+                new Service(_filtersService, grpc.Filters.BindService(_filtersService), grpc.Filters.Descriptor),
+                new Service(_eventHandlers, grpc.EventHandlers.BindService(_eventHandlers), grpc.EventHandlers.Descriptor),
             };
         }
     }
