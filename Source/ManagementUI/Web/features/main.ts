e10028188b3b03624ec4d4cae27848731a9d725d
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-07 18:44:26 +0100

    Setting up cross cuttingly the fetch client

--- Source/ManagementUI/Web/features/main.ts
+++ Source/ManagementUI/Web/features/main.ts
@@ -2,9 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { Aurelia } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
 import { PLATFORM } from 'aurelia-pal';
 import 'aurelia-polyfills';
 import environment from './environment';
+import { Globals } from './globals';
 
 export function configure(aurelia: Aurelia) {
     aurelia.use
@@ -12,6 +14,22 @@ export function configure(aurelia: Aurelia) {
         .feature(PLATFORM.moduleName('resources/index'))
         .plugin(PLATFORM.moduleName('aurelia-animator-css'))
         .plugin(PLATFORM.moduleName('@dunite/au-office-ui'));
+
+    const globals = new Globals()
+    aurelia.container.registerInstance<Globals>(Globals, globals);
+
+    aurelia.container.registerHandler<HttpClient>(HttpClient, () => {
+        const client = new HttpClient();
+        client.configure(config => {
+            config.withInterceptor({
+                request(request) {
+                    request.headers.append('Tenant-ID', globals.currentTenant.toString());
+                    return request;
+                }
+            })
+        });
+        return client;
+    });
 
     aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
