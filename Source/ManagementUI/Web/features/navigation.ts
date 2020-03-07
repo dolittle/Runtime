227082d3684c760c2eee3a0ab868afa8bde1c8cf
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-07 18:44:35 +0100

    Adding Tenant drop-down

--- Source/ManagementUI/Web/features/navigation.ts
+++ Source/ManagementUI/Web/features/navigation.ts
@@ -1,7 +1,19 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { Guid } from '@dolittle/rudiments';
+
+import { autoinject } from 'aurelia-dependency-injection';
+import { HttpClient } from 'aurelia-fetch-client';
+
+import { Globals } from './globals';
+
+import { IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
+
+@autoinject
 export class Navigation {
+    tenants: any[] = [];
+
     groups = [
         {
             isExpanded: true,
@@ -23,6 +35,11 @@ export class Navigation {
         {
             isExpanded: true,
             links: [
+                {
+                    key: 'aggregates',
+                    name: 'Aggregates',
+                    url: '#'
+                },
                 {
                     key: 'log',
                     name: 'Event Log',
@@ -54,10 +71,45 @@ export class Navigation {
                     key: 'observers',
                     name: 'Observers',
                     url: '#'
+                },
+                {
+                    key: 'identities',
+                    name: 'Identities',
+                    url: '#'
                 }
             ],
             name: 'TimeSeries',
             url: '#',
         }
     ];
+
+    constructor(
+        private _httpClient: HttpClient,
+        private _globals: Globals) {
+    }
+
+    tenantSelected(event: any, option?: IDropdownOption, index?: number): void {
+        const tenant = Guid.parse(option?.key as string);
+        this._globals.currentTenant = tenant;
+    }
+
+    async refreshTenants() {
+        await this.loadTenants();
+    }
+
+    async attached() {
+        await this.loadTenants();
+    }
+
+    async loadTenants() {
+        const result = await this._httpClient.get('/api/Tenants');
+        const tenants = await result.json() as any as any[];
+
+        tenants.forEach((tenant: any) => {
+            this.tenants.push({
+                key: tenant.value,
+                text: tenant.value,
+            });
+        });
+    }
 }
