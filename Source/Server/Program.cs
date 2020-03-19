d996a0e18915e60222828bdc99b22c0195c24b3c
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-19 07:49:21 +0100

    Putting Grpc-Web

--- Source/Server/Program.cs
+++ Source/Server/Program.cs
@@ -2,14 +2,36 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Dolittle.Runtime.Server
 {
     static class Program
     {
-        static async Task Main()
-        {
-            await Bootloader.Start().ConfigureAwait(false);
-        }
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args">Arguments for the process.</param>
+        public static async Task Main(string[] args) =>
+            await CreateHostBuilder(args)
+                .Build()
+                .RunAsync().ConfigureAwait(false);
+
+        /// <summary>
+        /// Create a host builder.
+        /// </summary>
+        /// <param name="args">Arguments for the process.</param>
+        /// <returns>Host builder to build and run.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseEnvironment("Development")
+                        .UseStartup<Startup>();
+                });
     }
 }
