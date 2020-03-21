3de2aa8aacf1904158431f602948252f32fc7ba8
Sindre Wilting <sindrewilting@gmail.com>
2020-03-21 12:30:45 +0100

    Public events filter service

---
+++ Source/EventHorizon/Producer/Filter/IWriteEventsToPublicStreams.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Events.Streams;
+
+namespace Dolittle.Runtime.EventHorizon.Producer.Filter
+{
+    /// <summary>
+    /// Defines a system that can write events to public event streams.
+    /// </summary>
+    public interface IWriteEventsToPublicStreams : IWriteEventsToStreams
+    {
+        /// <summary>
+        /// Writes an event to a public stream.
+        /// </summary>
+        /// <param name="event">The <see cref="CommittedEvent" />.</param>
+        /// <param name="streamId">The <see cref="StreamId" />.</param>
+        /// <param name="partitionId">The <see cref="PartitionId" />.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
+        /// <returns>A <see cref="Task"/> representing whether the event was successfully written to the event store.</returns>
+        Task Write(CommittedEvent @event, StreamId streamId, PartitionId partitionId, CancellationToken cancellationToken = default);
+    }
+}
