bf0fbb8658ea68f6d5e934fa12619cd9f11b430a
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-23 12:18:21 +0100

    Adding log page

--- Source/ManagementUI/features/app.ts
+++ Source/ManagementUI/features/app.ts
@@ -34,6 +34,7 @@ export class App {
             { route: ['', '/'], name: 'Index', moduleId: PLATFORM.moduleName('index'), nav: true },
             { route: 'general/overview', name: 'General Overview', moduleId: PLATFORM.moduleName('general/overview'), nav: true },
             { route: 'general/metrics', name: 'Metrics', moduleId: PLATFORM.moduleName('general/metrics'), nav: true },
+            { route: 'general/logs', name: 'Logs', moduleId: PLATFORM.moduleName('general/logs'), nav: true },
             { route: 'connections/runtimes', name: 'Runtime Connections', moduleId: PLATFORM.moduleName('connections/runtimes'), nav: true },
             { route: 'connections/heads', name: 'Connected Heads', moduleId: PLATFORM.moduleName('connections/heads'), nav: true }
         ]);
