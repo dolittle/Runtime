4b6fae2416e4424e4580593b3baa27336ad8935c
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-03-25 16:50:03 +0100

    Make FilterForStreamAlreadyRegistered an RpcException - so we get a nice message in Head

--- Source/Events.Processing/Filters/FilterForStreamAlreadyRegistered.cs
+++ Source/Events.Processing/Filters/FilterForStreamAlreadyRegistered.cs
@@ -2,20 +2,21 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Dolittle.Runtime.Events.Streams;
+using Grpc.Core;
 
 namespace Dolittle.Runtime.Events.Processing.Filters
 {
     /// <summary>
     /// Exception that gets thrown when multiple filters are registered for the same <see cref="StreamId" />.
     /// </summary>
-    public class FilterForStreamAlreadyRegistered : System.Exception
+    public class FilterForStreamAlreadyRegistered : RpcException
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="FilterForStreamAlreadyRegistered"/> class.
         /// </summary>
         /// <param name="streamId">The <see cref="StreamId" />.</param>
         public FilterForStreamAlreadyRegistered(StreamId streamId)
-            : base($"There has already been registered a filter for stream '{streamId}'")
+            : base(new Status(StatusCode.AlreadyExists, $"There has already been registered a filter for stream '{streamId}'"))
         {
         }
     }
