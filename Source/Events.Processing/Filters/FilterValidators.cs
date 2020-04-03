93feb92b4de4822d4ab7ffd9e6053ae1da96d60b
Sindre Wilting <sindrewilting@gmail.com>
2020-04-03 13:10:51 +0200

    Find instance of ICanValidateFilterFor by using the ITypeFinder

--- Source/Events.Processing/Filters/FilterValidators.cs
+++ Source/Events.Processing/Filters/FilterValidators.cs
@@ -1,11 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Dolittle.DependencyInversion;
 using Dolittle.Logging;
+using Dolittle.Types;
 
 namespace Dolittle.Runtime.Events.Processing.Filters
 {
@@ -14,38 +15,55 @@ namespace Dolittle.Runtime.Events.Processing.Filters
     /// </summary>
     public class FilterValidators : IFilterValidators
     {
+        readonly ITypeFinder _typeFinder;
         readonly IContainer _container;
         readonly ILogger _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FilterValidators"/> class.
         /// </summary>
+        /// <param name="typeFinder">The <see cref="ITypeFinder" />.</param>
         /// <param name="container">The <see cref="IContainer" />.</param>
         /// <param name="logger">The <see cref="ILogger" />.</param>
-        public FilterValidators(IContainer container, ILogger logger)
+        public FilterValidators(ITypeFinder typeFinder, IContainer container, ILogger logger)
         {
+            _typeFinder = typeFinder;
             _container = container;
             _logger = logger;
         }
 
         /// <inheritdoc/>
-        public Task Validate<TDefinition>(IFilterProcessor<TDefinition> filter, CancellationToken cancellationToken = default)
+        public Task<FilterValidationResult> Validate<TDefinition>(IFilterProcessor<TDefinition> filter, CancellationToken cancellationToken = default)
             where TDefinition : IFilterDefinition
         {
-            _logger.Debug($"Validating filter with filter definition type {typeof(TDefinition).FullName}");
-            ICanValidateFilterFor<TDefinition> validator;
-            try
+            _logger.Trace($"Finding validator for filter '{filter.Definition.TargetStream}'");
+
+            if (TryGetValidatorFor<TDefinition>(out var validator))
             {
-                validator = _container.Get<ICanValidateFilterFor<TDefinition>>();
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, $"Could not resolve a filter validator for filter with definition type {typeof(TDefinition).FullName}");
-                throw new CannotValidateFilterWithDefinitionType(typeof(TDefinition));
+                _logger.Trace($"Validating filter '{filter.Definition.TargetStream}'");
+                return validator.Validate(filter, cancellationToken);
             }
 
-            if (validator == null) throw new CannotValidateFilterWithDefinitionType(typeof(TDefinition));
-            return validator.Validate(filter, cancellationToken);
+            return Task.FromResult(new FilterValidationResult());
+        }
+
+        bool TryGetValidatorFor<TDefinition>(out ICanValidateFilterFor<TDefinition> validator)
+            where TDefinition : IFilterDefinition
+        {
+            var implementations = _typeFinder.FindMultiple(typeof(ICanValidateFilterFor<TDefinition>));
+            if (implementations.Any())
+            {
+                if (implementations.Count() > 1)
+                {
+                    _logger.Warning($"There are multiple validators that can validate filter defintion of type {typeof(TDefinition).FullName}:\n{string.Join("\n", implementations.Select(_ => _.FullName))}\nUsing the first validator.");
+                }
+
+                validator = _container.Get(implementations.First()) as ICanValidateFilterFor<TDefinition>;
+                return true;
+            }
+
+            validator = null;
+            return false;
         }
     }
 }
