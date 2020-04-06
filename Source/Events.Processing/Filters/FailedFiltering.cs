9b1d5bf92ecc480e1b590b897a5a73301b51dbec
Sindre Wilting <sindrewilting@gmail.com>
2020-04-06 16:12:06 +0200

    Represnt Successful and Failed processing

---
+++ Source/Events.Processing/Filters/FailedFiltering.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Runtime.Events.Streams;
+
+namespace Dolittle.Runtime.Events.Processing.Filters
+{
+    /// <summary>
+    /// Represents a failed <see cref="IFilterResult" />.
+    /// </summary>
+    public class FailedFiltering : FailedProcessing, IFilterResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FailedFiltering"/> class.
+        /// </summary>
+        /// <param name="reason">The reason for failure.</param>
+        public FailedFiltering(string reason)
+            : base(reason)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FailedFiltering"/> class.
+        /// </summary>
+        /// <param name="reason">The reason for failure.</param>
+        /// <param name="retry">Whether to retry processing.</param>
+        /// <param name="retryTimeout">The retry timout <see cref="TimeSpan" />.</param>
+        public FailedFiltering(string reason, bool retry, TimeSpan retryTimeout)
+            : base(reason, retry, retryTimeout)
+        {
+        }
+
+        /// <inheritdoc/>
+        public bool IsIncluded => false;
+
+        /// <inheritdoc/>
+        public PartitionId Partition => PartitionId.NotSet;
+    }
+}
---
+++ Source/Events.Processing/Filters/SuccessfulFiltering.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Dolittle.Runtime.Events.Streams;
+
+namespace Dolittle.Runtime.Events.Processing.Filters
+{
+    /// <summary>
+    /// Represents a successful <see cref="IFilterResult" />.
+    /// </summary>
+    public class SuccessfulFiltering : SuccessfulProcessing, IFilterResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SuccessfulFiltering"/> class.
+        /// </summary>
+        /// <param name="isIncluded">Is event included in filter.</param>
+        /// <param name="partitionId">The <see cref="PartitionId" /> />.</param>
+        public SuccessfulFiltering(bool isIncluded, PartitionId partitionId)
+        {
+            IsIncluded = isIncluded;
+            Partition = partitionId;
+        }
+
+        /// <inheritdoc />
+        public bool IsIncluded { get; }
+
+        /// <inheritdoc />
+        public PartitionId Partition { get; }
+    }
+}
