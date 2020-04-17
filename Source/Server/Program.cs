9aee2a853421d1555db00c1ccbb7097ed723512f
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-17 20:08:15 +0200

    Bind to all networks

--- Source/Server/Program.cs
+++ Source/Server/Program.cs
@@ -37,7 +37,7 @@ namespace Dolittle.Runtime.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
-                        .UseUrls("http://0.0.0.0:8001")
+                        .UseUrls("http://*:8001")
                         .UseEnvironment("Development")
                         .UseStartup<Startup>();
                 });
