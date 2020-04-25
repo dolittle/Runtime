efba7f4fccbaf107e0cd31ac1eba6948f06e5c0b
Sindre Wilting <sindrewilting@gmail.com>
2020-04-25 10:57:58 +0200

    Fix code based on PR review

--- Source/Tenancy/PerformActionOnAllTenants.cs
+++ Source/Tenancy/PerformActionOnAllTenants.cs
@@ -10,26 +10,44 @@ using Dolittle.Tenancy;
 namespace Dolittle.Runtime.Tenancy
 {
     /// <summary>
-    /// Represents an implementation of <see cref="IPerformAsynchronousActionOnAllTenants" />.
+    /// Represents an implementation of <see cref="IPerformActionOnAllTenants" />.
     /// </summary>
-    public class AsynchronousActionOnAllTenantsPerformer : IPerformAsynchronousActionOnAllTenants
+    public class PerformActionOnAllTenants : IPerformActionOnAllTenants
     {
         readonly ITenants _tenants;
         readonly IExecutionContextManager _executionContextManager;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="AsynchronousActionOnAllTenantsPerformer"/> class.
+        /// Initializes a new instance of the <see cref="PerformActionOnAllTenants"/> class.
         /// </summary>
         /// <param name="tenants">The <see cref="ITenants" />.</param>
         /// <param name="executionContextManager">The <see cref="IExecutionContextManager" />.</param>
-        public AsynchronousActionOnAllTenantsPerformer(ITenants tenants, IExecutionContextManager executionContextManager)
+        public PerformActionOnAllTenants(ITenants tenants, IExecutionContextManager executionContextManager)
         {
             _tenants = tenants;
             _executionContextManager = executionContextManager;
         }
 
         /// <inheritdoc/>
-        public async Task Perform(Func<TenantId, Task> action)
+        public void Perform(Action<TenantId> action)
+        {
+            var originalExecutionContext = _executionContextManager.Current;
+            try
+            {
+                foreach (var tenant in _tenants.All.ToArray())
+                {
+                    _executionContextManager.CurrentFor(tenant);
+                    action(tenant);
+                }
+            }
+            finally
+            {
+                _executionContextManager.CurrentFor(originalExecutionContext);
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task PerformAsync(Func<TenantId, Task> action)
         {
             var originalExecutionContext = _executionContextManager.Current;
             try
