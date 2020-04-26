c59c9dfa07cf53bb560b53e3479ab753e89eb99e
Sindre Wilting <sindrewilting@gmail.com>
2020-04-26 16:06:33 +0200

    Fix class doc

--- Source/Events.Processing/Streams/Unpartitioned/StreamProcessorState.cs
+++ Source/Events.Processing/Streams/Unpartitioned/StreamProcessorState.cs
@@ -2,14 +2,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using Dolittle.Concepts;
 using Dolittle.Runtime.Events.Store.Streams;
 
 namespace Dolittle.Runtime.Events.Processing.Streams.Unpartitioned
 {
     /// <summary>
-    /// Represents a combination of <see cref="StreamPosition" /> and <see cref="IDictionary{PartitionId, FailingPartitionState}">states of failing partitions</see> that represents the state of an <see cref="StreamProcessor" />.
+    /// Represents the state of an <see cref="StreamProcessor" />.
     /// </summary>
     public class StreamProcessorState : Value<StreamProcessorState>, IStreamProcessorState
     {
