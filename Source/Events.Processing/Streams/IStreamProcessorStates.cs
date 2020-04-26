6aab71a356e7f73970223994d0013b6c68bab23c
Sindre Wilting <sindrewilting@gmail.com>
2020-04-26 13:39:09 +0200

    Simplify the base stream processor state repository. It should only know how to get a stream processor state

--- Source/Events.Processing/Streams/IStreamProcessorStates.cs
+++ Source/Events.Processing/Streams/IStreamProcessorStates.cs
@@ -1,39 +1,30 @@
-// Copyright (c) Dolittle. All rights reserved.
+﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Threading;
 using System.Threading.Tasks;
-using Dolittle.Runtime.Events.Store.Streams;
 
 namespace Dolittle.Runtime.Events.Processing.Streams
 {
     /// <summary>
-    /// Defines a system that knows how to handle <see cref="StreamProcessorState" />.
+    /// Defines a system that knows about persisted <see cref="IStreamProcessorState" >stream processor states</see>.
     /// </summary>
     public interface IStreamProcessorStates
     {
         /// <summary>
-        /// Gets the <see cref="IFailingPartitions" />.
+        /// Gets a value indicating whether there is a persisted <see cref="IStreamProcessorState" /> for the given <see cref="StreamProcessorId" />.
         /// </summary>
-        IFailingPartitions FailingPartitions { get; }
+        /// <param name="streamProcessorId">The unique <see cref="StreamProcessorId" /> representing the <see cref="StreamProcessor"/>.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
+        /// <returns>A <see cref="Task" /> that, when resolved, returns a value indicating whether there is a persisted <see cref="IStreamProcessorState" />.</returns>
+        Task<bool> HasFor(StreamProcessorId streamProcessorId, CancellationToken cancellationToken);
 
         /// <summary>
-        /// Gets the stored <see cref="StreamProcessorState" />.
+        /// Gets the <see cref="IStreamProcessorState" /> for the given <see cref="StreamProcessorId" />.
         /// </summary>
-        /// <param name="streamProcessorId">The <see cref="StreamProcessorId" />.</param>
+        /// <param name="streamProcessorId">The unique <see cref="StreamProcessorId" /> representing the <see cref="StreamProcessor"/>.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
-        /// <returns>The stored <see cref="StreamProcessorState" />.</returns>
-        Task<StreamProcessorState> GetStoredStateFor(StreamProcessorId streamProcessorId, CancellationToken cancellationToken);
-
-        /// <summary>
-        /// Handles the processing an event for a <see cref="StreamProcessor" /> by changing the <see cref="StreamProcessorState" /> of the <see cref="StreamProcessor" /> according to the result of the processing.
-        /// </summary>
-        /// <param name="streamProcessorId">The <see cref="StreamProcessorId" />.</param>
-        /// <param name="eventProcessor">The <see cref="IEventProcessor" />.</param>
-        /// <param name="streamEvent">The <see cref="StreamEvent" />.</param>
-        /// <param name="currentState">The current <see cref="StreamProcessorState" />.</param>
-        /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
-        /// <returns>The new <see cref="StreamProcessorState" />.</returns>
-        Task<StreamProcessorState> ProcessEventAndChangeStateFor(StreamProcessorId streamProcessorId, IEventProcessor eventProcessor, StreamEvent streamEvent, StreamProcessorState currentState, CancellationToken cancellationToken);
+        /// <returns>The persisted <see cref="IStreamProcessorState" />.</returns>
+        Task<IStreamProcessorState> GetFor(StreamProcessorId streamProcessorId, CancellationToken cancellationToken);
     }
 }
