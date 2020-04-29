07a4b0047495c49090976c7b236a70537549243f
Sindre Wilting <sindrewilting@gmail.com>
2020-04-29 13:10:26 +0200

    Remvoe stream processors registrations and change IStreamProcessorsInterface

--- Source/Events.Processing/Streams/IStreamProcessors.cs
+++ Source/Events.Processing/Streams/IStreamProcessors.cs
@@ -3,6 +3,7 @@
 
 using System.Threading;
 using System.Threading.Tasks;
+using Dolittle.Runtime.Async;
 using Dolittle.Runtime.Events.Store.Streams;
 
 namespace Dolittle.Runtime.Events.Processing.Streams
@@ -18,7 +19,7 @@ namespace Dolittle.Runtime.Events.Processing.Streams
         /// <param name="streamDefinition">The <see cref="StreamDefinition" /> of the stream the <see cref="AbstractStreamProcessor" /> will be registered on.</param>
         /// <param name="eventProcessor">The <see cref="IEventProcessor" />.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
-        /// <returns>A <see cref="Task" /> that, when resolve, returns the <see cref="StreamProcessorRegistration"/>.</returns>
-        Task<StreamProcessorRegistration> Register(StreamDefinition streamDefinition, IEventProcessor eventProcessor, CancellationToken cancellationToken);
+        /// <returns>A <see cref="Task" /> that, when resolve, returns the <see cref="Try{TResult}" /> for <see cref="AbstractStreamProcessor"/>.</returns>
+        Task<Try<AbstractStreamProcessor>> TryRegister(StreamDefinition streamDefinition, IEventProcessor eventProcessor, CancellationToken cancellationToken);
     }
 }
