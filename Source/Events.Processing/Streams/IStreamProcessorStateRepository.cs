5b351406006e7bb2eb3e4e4b4e460677fa1be2a3
Sindre Wilting <sindrewilting@gmail.com>
2020-04-29 13:09:24 +0200

    Change interace name

--- Source/Events.Processing/Streams/IStreamProcessorStateRepository.cs
+++ Source/Events.Processing/Streams/IStreamProcessorStateRepository.cs
@@ -3,21 +3,22 @@
 
 using System.Threading;
 using System.Threading.Tasks;
+using Dolittle.Runtime.Async;
 
 namespace Dolittle.Runtime.Events.Processing.Streams
 {
     /// <summary>
-    /// Defines a system that knows about persisted <see cref="IStreamProcessorState" >stream processor states</see>.
+    /// Defines a system repository for <see cref="IStreamProcessorState" >stream processor states</see>.
     /// </summary>
-    public interface IStreamProcessorStates
+    public interface IStreamProcessorStateRepository
     {
         /// <summary>
         /// Gets the <see cref="IStreamProcessorState" /> for the given <see cref="StreamProcessorId" />.
         /// </summary>
         /// <param name="streamProcessorId">The unique <see cref="StreamProcessorId" /> representing the <see cref="AbstractStreamProcessor"/>.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
-        /// <returns>A <see cref="Task" /> that, when resolved, returns the tuple with a <see cref="bool" /> and the persisted <see cref="IStreamProcessorState" /> if exists.</returns>
-        Task<(bool success, IStreamProcessorState streamProcessorState)> TryGetFor(StreamProcessorId streamProcessorId, CancellationToken cancellationToken);
+        /// <returns>A <see cref="Task" /> that, when resolved, returns <see cref="Try{TResult}" />.</returns>
+        Task<Try<IStreamProcessorState>> TryGetFor(StreamProcessorId streamProcessorId, CancellationToken cancellationToken);
 
         /// <summary>
         /// Persist the <see cref="IStreamProcessorState" /> for <see cref="StreamProcessorId" />.
