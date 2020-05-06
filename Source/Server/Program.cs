951864cdb3dc74031ded43d253e7dd45caf08bfa
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-06 12:35:04 +0200

    Adding output of unhandled exceptions

--- Source/Server/Program.cs
+++ Source/Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading.Tasks;
 using Dolittle.Hosting.Microsoft;
 using Microsoft.AspNetCore.Hosting;
@@ -15,10 +16,14 @@ namespace Dolittle.Runtime.Server
         /// Main method.
         /// </summary>
         /// <param name="args">Arguments for the process.</param>
-        public static async Task Main(string[] args) =>
+        public static async Task Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptions;
+
             await CreateHostBuilder(args)
                 .Build()
                 .RunAsync().ConfigureAwait(false);
+        }
 
         /// <summary>
         /// Create a host builder.
@@ -41,6 +46,32 @@ namespace Dolittle.Runtime.Server
                         .UseEnvironment("Development")
                         .UseStartup<Startup>();
                 });
+        }
+
+        static void UnhandledExceptions(object sender, UnhandledExceptionEventArgs args)
+        {
+            if (args.ExceptionObject is Exception exception)
+            {
+                Console.WriteLine("************ BEGIN UNHANDLED EXCEPTION ************");
+                PrintExceptionInfo(exception);
+
+                while (exception.InnerException != null)
+                {
+                    Console.WriteLine("\n------------ BEGIN INNER EXCEPTION ------------");
+                    PrintExceptionInfo(exception.InnerException);
+                    exception = exception.InnerException;
+                    Console.WriteLine("------------ END INNER EXCEPTION ------------\n");
+                }
+
+                Console.WriteLine("************ END UNHANDLED EXCEPTION ************ ");
+            }
+        }
+
+        static void PrintExceptionInfo(Exception exception)
+        {
+            Console.WriteLine($"Exception type: {exception.GetType().FullName}");
+            Console.WriteLine($"Exception message: {exception.Message}");
+            Console.WriteLine($"Stack Trace: {exception.StackTrace}");
         }
     }
 }
