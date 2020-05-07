2fc580c39a1cd7aabd99ecda8291db78c3666ae3
Joel Hoisko <joel.hoisko@gmail.com>
2020-05-07 13:35:55 +0200

    Add extensions for SubscriptionState

---
+++ Source/Events.Store.MongoDB/Processing/Streams/SubscriptionStateExtensions.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Dolittle.Runtime.Events.Processing.Streams;
+
+namespace Dolittle.Runtime.Events.Store.MongoDB.Processing.Streams
+{
+    /// <summary>
+    /// Extension methods for <see cref="SubscriptionState" />.
+    /// </summary>
+    public static class SubscriptionStateExtensions
+    {
+        /// <summary>
+        /// Converts the <see cref="SubscriptionState" /> to the runtime representation of <see cref="Runtime.Events.Processing.Streams.StreamProcessorState" />
+        /// as the SubscriptionStates are represented by an unpartioned StreamProcesssorState in the runtime.
+        /// </summary>
+        /// <param name="state">The <see cref="StreamProcessorState" />.</param>
+        /// <returns>The converted <see cref="Runtime.Events.Processing.Streams.Partitioned.StreamProcessorState" />.</returns>
+        public static Runtime.Events.Processing.Streams.StreamProcessorState ToUnpartitionedStreamProcessorState(this SubscriptionState state) =>
+            new Runtime.Events.Processing.Streams.StreamProcessorState(
+                state.Position,
+                state.FailureReason,
+                state.RetryTime,
+                state.ProcessingAttempts);
+
+        /// <summary>
+        /// Converts the <see cref="AbstractSubscriptionState" /> to the runtime representation of
+        /// <see cref="Runtime.Events.Processing.Streams.StreamProcessorState" />.
+        /// </summary>
+        /// <param name="state">The <see cref="AbstractSubscriptionState" />.</param>
+        /// <returns>The converted <see cref="IStreamProcessorState" />.</returns>
+        public static IStreamProcessorState ToRuntimeRepresentation(this AbstractSubscriptionState state) =>
+            (state as SubscriptionState).ToUnpartitionedStreamProcessorState() as IStreamProcessorState;
+    }
+}
