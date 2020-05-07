950319b1902e6daabbd055061621e3a4383e27fb
Joel Hoisko <joel.hoisko@gmail.com>
2020-05-07 13:36:17 +0200

    Fix TryGetFor to handle both possible id's

--- Source/Events.Store.MongoDB/Processing/Streams/StreamProcessorStates.cs
+++ Source/Events.Store.MongoDB/Processing/Streams/StreamProcessorStates.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Dolittle.Logging;
 using Dolittle.Runtime.Async;
+using Dolittle.Runtime.EventHorizon;
 using Dolittle.Runtime.Events.Processing.Streams;
 using MongoDB.Driver;
 
@@ -16,6 +17,7 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Processing.Streams
     public class StreamProcessorStates : IStreamProcessorStateRepository
     {
         readonly FilterDefinitionBuilder<AbstractStreamProcessorState> _streamProcessorFilter;
+        readonly FilterDefinitionBuilder<AbstractSubscriptionState> _subscriptionFilter;
         readonly EventStoreConnection _connection;
         readonly ILogger _logger;
 
@@ -27,33 +29,49 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Processing.Streams
         public StreamProcessorStates(EventStoreConnection connection, ILogger logger)
         {
             _streamProcessorFilter = Builders<AbstractStreamProcessorState>.Filter;
+            _subscriptionFilter = Builders<AbstractSubscriptionState>.Filter;
             _connection = connection;
             _logger = logger;
         }
 
-        /// <inheritdoc/>
-        public Task<Try<IStreamProcessorState>> TryGetFor(IStreamProcessorId streamProcessorId, CancellationToken cancellationToken)
+        /// <summary>
+        /// Gets the <see cref="IStreamProcessorState" /> for the given <see cref="IStreamProcessorId" /> from the correct
+        /// collection, either <see cref="SubscriptionState" /> or <see cref="StreamProcessorState" />.
+        /// </summary>
+        /// <param name="id">The unique <see cref="IStreamProcessorId" /> representing either the <see cref="AbstractScopedStreamProcessor"/>
+        /// or <see cref="SubscriptionId" />.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken" />.</param>
+        /// <returns>A <see cref="Task" /> that, when resolved, returns <see cref="Try{TResult}" />.</returns>
+        public async Task<Try<IStreamProcessorState>> TryGetFor(IStreamProcessorId id, CancellationToken cancellationToken)
         {
             try
             {
-                return Task.FromResult(new Try<IStreamProcessorState>(false, null));
-
-                // TODO: There are now two different collections for stream proecssor states. One for StreamProcessorId (Events.Processing) and one for SubscriptionId (EventHorizon)
-                // var states = await _connection.GetStreamProcessorStateCollection(streamProcessorId.ScopeId, cancellationToken).ConfigureAwait(false);
-                // var persistedState = await states.Find(
-                //     _streamProcessorFilter.Eq(_ => _.ScopeId, streamProcessorId.ScopeId.Value)
-                //         & _streamProcessorFilter.Eq(_ => _.EventProcessorId, streamProcessorId.EventProcessorId.Value)
-                //         & _streamProcessorFilter.Eq(_ => _.SourceStreamId, streamProcessorId.SourceStreamId.Value))
-                //     .FirstOrDefaultAsync(cancellationToken)
-                //     .ConfigureAwait(false);
-                // if (persistedState != null)
-                // {
-                //     return (true, persistedState.ToRuntimeRepresentation());
-                // }
-                // else
-                // {
-                //     return (false, null);
-                // }
+                if (id is SubscriptionId subscriptionId)
+                {
+                    var states = await _connection.GetSubscriptionStateCollection(subscriptionId.ScopeId, cancellationToken).ConfigureAwait(false);
+                    var persistedState = await states.Find(
+                        _subscriptionFilter.Eq(_ => _.ConsumerTenantId, subscriptionId.ConsumerTenantId.Value)
+                            & _subscriptionFilter.Eq(_ => _.ProducerMicroserviceId, subscriptionId.ProducerMicroserviceId.Value)
+                            & _subscriptionFilter.Eq(_ => _.ProducerTenantId, subscriptionId.ProducerTenantId.Value)
+                            & _subscriptionFilter.Eq(_ => _.ScopeId, subscriptionId.ScopeId.Value)
+                            & _subscriptionFilter.Eq(_ => _.StreamId, subscriptionId.StreamId.Value)
+                            & _subscriptionFilter.Eq(_ => _.PartitionId, subscriptionId.PartitionId.Value))
+                        .FirstOrDefaultAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                    return (persistedState != null) ? (true, persistedState.ToRuntimeRepresentation()) : (false, null);
+                }
+                else
+                {
+                    var streamProcessorId = id as StreamProcessorId;
+                    var states = await _connection.GetStreamProcessorStateCollection(streamProcessorId.ScopeId, cancellationToken).ConfigureAwait(false);
+                    var persistedState = await states.Find(
+                        _streamProcessorFilter.Eq(_ => _.ScopeId, streamProcessorId.ScopeId.Value)
+                            & _streamProcessorFilter.Eq(_ => _.EventProcessorId, streamProcessorId.EventProcessorId.Value)
+                            & _streamProcessorFilter.Eq(_ => _.SourceStreamId, streamProcessorId.SourceStreamId.Value))
+                        .FirstOrDefaultAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                    return (persistedState != null) ? (true, persistedState.ToRuntimeRepresentation()) : (false, null);
+                }
             }
             catch (MongoWaitQueueFullException ex)
             {
@@ -62,20 +80,18 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Processing.Streams
         }
 
         /// <inheritdoc/>
-        public Task Persist(IStreamProcessorId streamProcessorId, IStreamProcessorState streamProcessorState, CancellationToken cancellationToken)
+        public async Task Persist(IStreamProcessorId streamProcessorId, IStreamProcessorState streamProcessorState, CancellationToken cancellationToken)
         {
             try
             {
-                return Task.CompletedTask;
-
                 // TODO: There are now two different collections for stream proecssor states. One for StreamProcessorId (Events.Processing) and one for SubscriptionId (EventHorizon)
-                // var states = await _connection.GetStreamProcessorStateCollection(streamProcessorId.ScopeId, cancellationToken).ConfigureAwait(false);
-                // var state = await states.Find(
-                //     _streamProcessorFilter.Eq(_ => _.ScopeId, streamProcessorId.ScopeId.Value)
-                //         & _streamProcessorFilter.Eq(_ => _.EventProcessorId, streamProcessorId.EventProcessorId.Value)
-                //         & _streamProcessorFilter.Eq(_ => _.SourceStreamId, streamProcessorId.SourceStreamId.Value))
-                //     .FirstOrDefaultAsync(cancellationToken)
-                //     .ConfigureAwait(false);
+                var states = await _connection.GetStreamProcessorStateCollection(streamProcessorId.ScopeId, cancellationToken).ConfigureAwait(false);
+                var state = await states.Find(
+                    _streamProcessorFilter.Eq(_ => _.ScopeId, streamProcessorId.ScopeId.Value)
+                        & _streamProcessorFilter.Eq(_ => _.EventProcessorId, streamProcessorId.EventProcessorId.Value)
+                        & _streamProcessorFilter.Eq(_ => _.SourceStreamId, streamProcessorId.SourceStreamId.Value))
+                    .FirstOrDefaultAsync(cancellationToken)
+                    .ConfigureAwait(false);
             }
             catch (MongoWaitQueueFullException ex)
             {
