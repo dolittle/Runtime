b92844699f95641bbfd70cc4f7e827f8978fabaf
Joel Hoisko <joel.hoisko@gmail.com>
2020-05-08 11:53:52 +0200

    Change to more descriptive exceptions

---
+++ Source/Events.Store.MongoDB/Processing/Streams/StreamProcessorStateOfUnsupportedType.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Runtime.Events.Processing.Streams;
+
+namespace Dolittle.Runtime.Events.Store.MongoDB.Processing.Streams
+{
+    /// <summary>
+    /// Exception that gets thrown when trying to use an unsuported <see cref="IStreamProcessorState"/> and <see cref="IStreamProcessorId"/> and .
+    /// </summary>
+    public class StreamProcessorStateOfUnsupportedType : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamProcessorStateOfUnsupportedType"/> class.
+        /// </summary>
+        /// <param name="id">The failed id.</param>
+        /// <param name="state">The failed state class.</param>
+        public StreamProcessorStateOfUnsupportedType(IStreamProcessorId id, IStreamProcessorState state)
+            : base($"Unsupported StreamProcessorState: {state} with IStreamProcessorId: {id}")
+        {
+        }
+    }
+}
