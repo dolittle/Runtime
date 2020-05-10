21f79a3480e0b71ea391d253ea7b0678b4e54ad8
Sindre Wilting <sindrewilting@gmail.com>
2020-05-10 10:33:02 +0200

    Add Exception to Try{TResult}

--- Source/Async/Try.cs
+++ Source/Async/Try.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Dolittle.Runtime.Async
 {
     /// <summary>
@@ -15,10 +17,12 @@ namespace Dolittle.Runtime.Async
         /// </summary>
         /// <param name="success">Whether the try-get operation was successful.</param>
         /// <param name="result">The result.</param>
-        public Try(bool success, TResult result)
+        /// <param name="exception">The optional <see cref="Exception" />.</param>
+        public Try(bool success, TResult result, Exception exception = default)
         {
             Success = success;
             Result = result;
+            Exception = exception;
         }
 
         /// <summary>
@@ -30,6 +34,11 @@ namespace Dolittle.Runtime.Async
         /// Gets the <typeparamref name="TResult">result</typeparamref>.
         /// </summary>
         public TResult Result { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Exception" /> that caused the operation to fail.
+        /// </summary>
+        public Exception Exception { get; }
 
         /// <summary>
         /// Implicitly convert <see cref="Try{TResult}" /> to <see cref="Try{TResult}.Success" />.
@@ -46,6 +55,13 @@ namespace Dolittle.Runtime.Async
         public static implicit operator TResult(Try<TResult> @try) => @try.Result;
 
         /// <summary>
+        /// Implicitly convert <see cref="Try{TResult}" /> to <see cref="Try{TResult}.Exception" />.
+        /// </summary>
+        /// <param name="try">The <see cref="Try{TResult}" /> to convert.</param>
+        /// <return><see cref="Try{TResult}.Exception" />.</return>
+        public static implicit operator Exception(Try<TResult> @try) => @try.Exception;
+
+        /// <summary>
         /// Implicitly convert <typeparamref name="TResult">result</typeparamref> to <see cref="Try{TResult}" />.
         /// </summary>
         /// <param name="result">The <typeparamref name="TResult">result</typeparamref> to convert.</param>
@@ -60,10 +76,24 @@ namespace Dolittle.Runtime.Async
         public static implicit operator Try<TResult>(bool success) => new Try<TResult>(success, null);
 
         /// <summary>
+        /// Implicitly convert <see cref="bool" /> to <see cref="Try{TResult}" />.
+        /// </summary>
+        /// <param name="exception">The <see cref="System.Exception" /> to convert.</param>
+        /// <return><see cref="Try{TResult}" />.</return>
+        public static implicit operator Try<TResult>(Exception exception) => new Try<TResult>(false, null, exception);
+
+        /// <summary>
         /// Implicitly convert <see cref="bool" /> and <typeparamref name="TResult" /> tuple to <see cref="Try{TResult}" />.
         /// </summary>
         /// <param name="try">The <see cref="bool" /> and <typeparamref name="TResult" /> tuple to convert.</param>
         /// <return><see cref="Try{TResult}" />.</return>
         public static implicit operator Try<TResult>((bool success, TResult result) @try) => new Try<TResult>(@try.success, @try.result);
+
+        /// <summary>
+        /// Implicitly convert <see cref="bool" />, <typeparamref name="TResult" /> and <see cref="System.Exception" /> tuple to <see cref="Try{TResult}" />.
+        /// </summary>
+        /// <param name="try">The <see cref="bool" />, <typeparamref name="TResult" /> and <see cref="System.Exception" /> tuple to convert.</param>
+        /// <return><see cref="Try{TResult}" />.</return>
+        public static implicit operator Try<TResult>((bool success, TResult result, Exception exception) @try) => new Try<TResult>(@try.success, @try.result, @try.exception);
     }
 }
