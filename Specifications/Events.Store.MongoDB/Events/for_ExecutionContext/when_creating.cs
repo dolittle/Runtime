e1fedee3c3b7a12963b0e729843b4ffafb0bf0ac
Sindre Wilting <sindrewilting@gmail.com>
2020-05-14 10:06:18 +0200

    Add more specs

---
+++ Specifications/Events.Store.MongoDB/Events/for_ExecutionContext/when_creating.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Machine.Specifications;
+
+namespace Dolittle.Runtime.Events.Store.MongoDB.Events.for_ExecutionContext
+{
+    public class when_creating
+    {
+        static Guid correlation;
+        static Guid microservice;
+        static Guid tenant;
+        static Version version;
+        static string environment;
+        static ExecutionContext result;
+
+        Establish context = () =>
+        {
+            correlation = Guid.NewGuid();
+            microservice = Guid.NewGuid();
+            tenant = Guid.NewGuid();
+            version = new Version(0, 0, 1, 0, "");
+            environment = "some environmen";
+        };
+
+        Because of = () => result = new ExecutionContext(
+            correlation,
+            microservice,
+            tenant,
+            version,
+            environment);
+
+        It should_have_the_correct_correlation = () => result.Correlation.ShouldEqual(correlation);
+        It should_have_the_correct_microservice = () => result.Microservice.ShouldEqual(microservice);
+        It should_have_the_correct_tenant = () => result.Tenant.ShouldEqual(tenant);
+        It should_have_the_correct_version = () => result.Version.ShouldEqual(version);
+        It should_have_the_correct_environment = () => result.Environment.ShouldEqual(environment);
+    }
+}
---
+++ Specifications/Events.Store.MongoDB/Events/for_StreamEvent/when_creating.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Machine.Specifications;
+using MongoDB.Bson;
+
+namespace Dolittle.Runtime.Events.Store.MongoDB.Events.for_StreamEvent
+{
+    public class when_creating
+    {
+        static ulong stream_position;
+        static Guid partition;
+        static ExecutionContext execution_context;
+        static StreamEventMetadata stream_event_metadata;
+        static AggregateMetadata aggregate_metadata;
+        static BsonDocument content;
+        static StreamEvent result;
+
+        Establish context = () =>
+        {
+            stream_position = 2;
+            partition = Guid.NewGuid();
+            execution_context = execution_contexts.create_store();
+            stream_event_metadata = metadata.random_stream_event_metadata;
+            aggregate_metadata = metadata.aggregate_metadata_from_non_aggregate_event;
+            content = BsonDocument.Parse("{\"something\": \"something\"}");
+        };
+
+        Because of = () => result = new StreamEvent(
+            stream_position,
+            partition,
+            execution_context,
+            stream_event_metadata,
+            aggregate_metadata,
+            content);
+
+        It should_have_the_correct_stream_position = () => result.StreamPosition.ShouldEqual(stream_position);
+        It should_have_the_correct_partition = () => result.Partition.ShouldEqual(partition);
+        It should_have_the_correct_execution_context = () => result.ExecutionContext.ShouldEqual(execution_context);
+        It should_have_the_correct_stream_event_metadata = () => result.Metadata.ShouldEqual(stream_event_metadata);
+        It should_have_the_correct_aggregate_metadata = () => result.Aggregate.ShouldEqual(aggregate_metadata);
+        It should_have_the_correct_content = () => result.Content.ShouldEqual(content);
+    }
+}
