1e6710cb393c79ec4181c501f82a8b9cce001667
Sindre Wilting <sindrewilting@gmail.com>
2020-05-15 13:22:02 +0200

    Fix build errors

--- Specifications/Events.Processing/Filters/EventHorizon/for_PublicFilterProcessor/given/a_public_event_filter.cs
+++ Specifications/Events.Processing/Filters/EventHorizon/for_PublicFilterProcessor/given/a_public_event_filter.cs
@@ -35,13 +35,13 @@ namespace Dolittle.Runtime.Events.Processing.Filters.EventHorizon.for_PublicFilt
                 false,
                 "");
 
-        protected static Mock<IReverseCallDispatcher<PublicFiltersClientToRuntimeMessage, FilterRuntimeToClientMessage, PublicFiltersRegistrationRequest, FilterRegistrationResponse, FilterEventRequest, PartitionedFilterResponse>> dispatcher;
+        protected static Mock<IReverseCallDispatcher<PublicFilterClientToRuntimeMessage, FilterRuntimeToClientMessage, PublicFilterRegistrationRequest, FilterRegistrationResponse, FilterEventRequest, PartitionedFilterResponse>> dispatcher;
 
         protected static PublicFilterProcessor filter;
 
         Establish context = () =>
         {
-            dispatcher = new Mock<IReverseCallDispatcher<PublicFiltersClientToRuntimeMessage, FilterRuntimeToClientMessage, PublicFiltersRegistrationRequest, FilterRegistrationResponse, FilterEventRequest, PartitionedFilterResponse>>();
+            dispatcher = new Mock<IReverseCallDispatcher<PublicFilterClientToRuntimeMessage, FilterRuntimeToClientMessage, PublicFilterRegistrationRequest, FilterRegistrationResponse, FilterEventRequest, PartitionedFilterResponse>>();
             filter = new PublicFilterProcessor(
                 new PublicFilterDefinition(Guid.NewGuid(), Guid.NewGuid()),
                 dispatcher.Object,
