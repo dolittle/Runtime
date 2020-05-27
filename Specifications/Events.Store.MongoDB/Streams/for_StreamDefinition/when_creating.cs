8d4c8cd096d3601b82df8494054715badc6f681f
Sindre Wilting <sindrewilting@gmail.com>
2020-05-27 09:56:53 +0200

    Make spec compatible

--- Specifications/Events.Store.MongoDB/Streams/for_StreamDefinition/when_creating.cs
+++ Specifications/Events.Store.MongoDB/Streams/for_StreamDefinition/when_creating.cs
@@ -3,7 +3,6 @@
 
 using System;
 using Dolittle.Runtime.Events.Store.MongoDB.Streams.Filters;
-using Dolittle.Runtime.Events.Store.Streams;
 using Machine.Specifications;
 
 namespace Dolittle.Runtime.Events.Store.MongoDB.Streams.for_StreamDefinition
@@ -19,7 +18,7 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Streams.for_StreamDefinition
         Establish context = () =>
         {
             stream_id = Guid.Parse("7e2be412-c950-48f3-bd23-66c46cba452a");
-            filter_definition = new FilterDefinition(stream_id, StreamId.EventLog, partitioned);
+            filter_definition = new RemoteFilterDefinition();
             partitioned = true;
             @public = false;
         };
@@ -29,6 +28,6 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Streams.for_StreamDefinition
         It should_have_the_correct_partitioned_value = () => result.Partitioned.ShouldEqual(partitioned);
         It should_have_the_correct_public_value = () => result.Public.ShouldEqual(@public);
         It should_have_the_correct_stream_id = () => result.StreamId.ShouldEqual(stream_id);
-        It should_have_the_correct_filter_definition = () => result.FilterDefinition.ShouldEqual(filter_definition);
+        It should_have_the_correct_filter_definition = () => result.Filter.ShouldEqual(filter_definition);
     }
 }
