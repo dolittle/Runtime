7a0e1cabcc1c954c3ee918699f03f06750aaaa6b
Sindre Wilting <sindrewilting@gmail.com>
2020-10-27 15:41:08 +0100

    Improve logging

--- Source/Events.Store.MongoDB/Aggregates/NextAggregateRootVersionMustBeGreaterThanCurrentVersion.cs
+++ Source/Events.Store.MongoDB/Aggregates/NextAggregateRootVersionMustBeGreaterThanCurrentVersion.cs
@@ -2,8 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using Dolittle.Artifacts;
 
-namespace Dolittle.Runtime.Events.Store.MongoDB
+namespace Dolittle.Runtime.Events.Store.MongoDB.Aggregates
 {
     /// <summary>
     /// Exception that gets thrown when attempting to decrease the version of an aggregate root instance.
@@ -13,10 +14,12 @@ namespace Dolittle.Runtime.Events.Store.MongoDB
         /// <summary>
         /// Initializes a new instance of the <see cref="NextAggregateRootVersionMustBeGreaterThanCurrentVersion"/> class.
         /// </summary>
+        /// <param name="eventSource">The <see cref="EventSourceId" />.</param>
+        /// <param name="aggregateRoot">The aggregate root id.</param>
         /// <param name="currentVersion">The current <see cref="AggregateRootVersion"/> of the aggregate root instance.</param>
         /// <param name="nextVersion">The <see cref="AggregateRootVersion"/> that was attempted to set on the aggregate root instance.</param>
-        public NextAggregateRootVersionMustBeGreaterThanCurrentVersion(AggregateRootVersion currentVersion, AggregateRootVersion nextVersion)
-            : base($"Next Aggregate Root instance version must be greater than '{currentVersion}', got '{nextVersion}'.")
+        public NextAggregateRootVersionMustBeGreaterThanCurrentVersion(EventSourceId eventSource, ArtifactId aggregateRoot, AggregateRootVersion currentVersion, AggregateRootVersion nextVersion)
+            : base($"Next aggregate root version of aggregate root {aggregateRoot} with event source {eventSource} must be greater than '{currentVersion}', but got '{nextVersion}'.")
         {
         }
     }
