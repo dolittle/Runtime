0a7bf1039da7198f8ca95b94248917eb1c5c9fa0
woksin <sindrewilting@gmail.com>
2021-01-14 11:42:08 +0100

    Interface for trying to get the time to retry processing based on the retry time of a stream processor's state

---
+++ Source/Events.Processing/Streams/ICanGetTimeToRetryFor.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Dolittle.Runtime.Events.Processing.Streams
+{
+    /// <summary>
+    /// Defines a system that can get the time when a stream processor should retry processing for a specific <see cref="IStreamProcessorState"/>.
+    /// </summary>
+    /// <typeparam name="T">A <see cref="Type" /> of <see cref="IStreamProcessorState" />.</typeparam>
+    public interface ICanGetTimeToRetryFor<T>
+        where T : class, IStreamProcessorState
+    {
+        /// <summary>
+        /// Tries to get the <see cref="TimeSpan" /> for when to retry processing. Outputs the maximum value <see cref="TimeSpan" /> if there is no retry time.
+        /// </summary>
+        /// <param name="streamProcessorState">The <typeparamref name="T"/>.</param>
+        /// <param name="timeToRetry">The <see cref="TimeSpan" /> for when to retry processsing a stream processor.</param>
+        /// <returns>A value indicating whether there is a retry time.</returns>
+        bool TryGetTimespanToRetry(T streamProcessorState, out TimeSpan timeToRetry);
+    }
+}
