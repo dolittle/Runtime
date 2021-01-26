9dbbb4acce1adc60676bdd5a00ebc38abe99f5a4
woksin <sindrewilting@gmail.com>
2021-01-26 15:18:09 +0100

    Remove Concepts from Artifacts

--- Source/Artifacts/ArtifactId.cs
+++ Source/Artifacts/ArtifactId.cs
@@ -2,21 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Dolittle.Runtime.Concepts;
 
 namespace Dolittle.Runtime.Artifacts
 {
     /// <summary>
     /// Represents the concept of a unique identifier for an artifact.
     /// </summary>
-    public class ArtifactId : ConceptAs<Guid>
+    public record ArtifactId(Guid Value)
     {
         /// <summary>
         /// Implicitly converts from a <see cref="Guid"/> to an <see cref="ArtifactId"/>.
         /// </summary>
         /// <param name="id">The <see cref="Guid"/> representation.</param>
         /// <returns>The converted <see cref="ArtifactId"/>.</returns>
-        public static implicit operator ArtifactId(Guid id) => new ArtifactId { Value = id };
+        public static implicit operator ArtifactId(Guid id) => new(id);
 
         /// <summary>
         /// Create a new <see cref="ArtifactId"/>.
