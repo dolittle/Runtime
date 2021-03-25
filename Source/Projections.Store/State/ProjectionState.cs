2562c4964ede00608dc27266e5b2a4dd0947ef73
woksin <sindrewilting@gmail.com>
2021-03-25 10:51:56 +0100

    Add implicit operator from string to ProjectionState

--- Source/Projections.Store/State/ProjectionState.cs
+++ Source/Projections.Store/State/ProjectionState.cs
@@ -10,5 +10,12 @@ namespace Dolittle.Runtime.Projections.Store.State
     /// </summary>
     /// <param name="Value">The state.</param>
     /// <typeparam name="string">The type of the concept.</typeparam>
-    public record ProjectionState(string Value) : ConceptAs<string>(Value);
+    public record ProjectionState(string Value) : ConceptAs<string>(Value)
+    {
+        /// <summary>
+        /// Implicitly convers the string to a <see cref="ProjectionState" />.
+        /// </summary>
+        /// <param name="state">The <see cref="ProjectionState" />.</param>
+        public static implicit operator ProjectionState(string state) => new(state);
+    }
 }
