08c37336a718c5334596d8a5aaf9be12706fed74
woksin <sindrewilting@gmail.com>
2021-03-29 10:26:34 +0200

    Add some implicit operators from string to guid concepts

--- Source/Projections.Store/ProjectionId.cs
+++ Source/Projections.Store/ProjectionId.cs
@@ -18,6 +18,12 @@ namespace Dolittle.Runtime.Projections.Store
         public static implicit operator ProjectionId(Guid projectionId) => new(projectionId);
 
         /// <summary>
+        /// Implicitly convert from a <see cref="string"/> to an <see cref="ProjectionId"/>.
+        /// </summary>
+        /// <param name="projectionId">ProjectionId as <see cref="string"/>.</param>
+        public static implicit operator ProjectionId(string projectionId) => new(Guid.Parse(projectionId));
+
+        /// <summary>
         /// Creates a new instance of <see cref="ProjectionId"/> with a unique id.
         /// </summary>
         /// <returns>A new <see cref="ProjectionId"/>.</returns>
