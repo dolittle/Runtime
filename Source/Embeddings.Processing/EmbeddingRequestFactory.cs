be08167e460210edc7e95c525004d459b378a650
woksin <sindrewilting@gmail.com>
2021-05-11 15:09:25 +0200

    Implment Create projection request on EmbeddingRequestFactory

--- Source/Embeddings.Processing/EmbeddingRequestFactory.cs
+++ Source/Embeddings.Processing/EmbeddingRequestFactory.cs
@@ -1,11 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 using System;
 using Dolittle.Runtime.Embeddings.Contracts;
 using Dolittle.Runtime.Embeddings.Store;
 using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Events.Processing.Projections;
+using Dolittle.Runtime.Protobuf;
 using Dolittle.Runtime.Projections.Store.State;
 using Dolittle.Runtime.Rudimentary;
 
@@ -18,9 +19,29 @@ namespace Dolittle.Runtime.Embeddings.Processing
     {
         /// <inheritdoc/>
         public EmbeddingRequest Create(ProjectionCurrentState current, UncommittedEvent @event)
-        {
-            throw new NotImplementedException();
-        }
+            => new()
+            {
+                Projection = new EmbeddingProjectRequest
+                {
+                    CurrentState = new Projections.Contracts.ProjectionCurrentState
+                    {
+                        Type = current.Type.ToProtobuf(),
+                        Key = current.Key,
+                        State = current.State
+                    },
+                    Event = new Events.Contracts.UncommittedEvent
+                    {
+                        Artifact = new Dolittle.Artifacts.Contracts.Artifact
+                        {
+                            Generation = @event.Type.Generation.Value,
+                            Id = @event.Type.Id.ToProtobuf()
+                        },
+                        Content = @event.Content,
+                        EventSourceId = @event.EventSource.ToProtobuf(),
+                        Public = @event.Public,
+                    }
+                }
+            };
 
         /// <inheritdoc/>
         public EmbeddingRequest Create(EmbeddingCurrentState current, ProjectionState desiredState)
