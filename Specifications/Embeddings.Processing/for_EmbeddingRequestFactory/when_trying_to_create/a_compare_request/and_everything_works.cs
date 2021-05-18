a4a9b5402cd5f943663a66f7aa6cf40edc37488f
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-05-18 14:28:29 +0200

    Update Specifications/Embeddings.Processing/for_EmbeddingRequestFactory/when_trying_to_create/a_compare_request/and_everything_works.cs
    Co-authored-by: Joel Hoisko <joel.hoisko@gmail.com>

--- Specifications/Embeddings.Processing/for_EmbeddingRequestFactory/when_trying_to_create/a_compare_request/and_everything_works.cs
+++ Specifications/Embeddings.Processing/for_EmbeddingRequestFactory/when_trying_to_create/a_compare_request/and_everything_works.cs
@@ -10,7 +10,6 @@ namespace Dolittle.Runtime.Embeddings.Processing.for_EmbeddingRequestFactory.whe
 {
     public class and_everything_works : given.all_dependencies
     {
-        static UncommittedEvent @event;
 
         static Try<Contracts.EmbeddingRequest> result;
 
