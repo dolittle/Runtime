564bb11ef972e85ad470a410d136770c2e836ceb
Einar Ingebrigtsen <einar@dolittle.com>
2021-06-01 20:04:35 +0200

    Start of showing event handlers

--- Source/ManagementUI/App.tsx
+++ Source/ManagementUI/App.tsx
@@ -1,36 +1,28 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { PrimaryButton } from '@fluentui/react';
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
 import { Layout } from './layouts/Layout';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.scss';
+import { EventHandlersOverview } from './eventHandlers/EventHandlersOverview';
 
-const query = gql`
-    query {
-    eventHandlers {
-        allForTenant(tenantId:"709f5739-0f3f-4cac-83bb-7ea3fed6d97d") {
-            id
-        }
-    }
-    }`;
 
 export const App = () => {
-    const { loading, error, data } = useQuery(query);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
 
     return (
         <>
             <Router>
                 <Layout />
                 <div className="content">
-                    <PrimaryButton>Hello there</PrimaryButton>
-
+                    
+                    <Route exact path="/">
+                        Home
+                    </Route>
+                    <Route path="/event-handlers">
+                        <EventHandlersOverview/>
+                    </Route>
                 </div>
             </Router>
         </>
---
+++ Source/ManagementUI/eventHandlers/EventHandlersOverview.tsx
@@ -0,0 +1,59 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { DetailsList, IColumn } from '@fluentui/react';
+
+const query = gql`
+    query {
+        eventHandlers {
+            allForTenant(tenantId:"709f5739-0f3f-4cac-83bb-7ea3fed6d97d") {
+                id
+                sourceStream
+                position
+                failed
+            }
+        }
+    }`;
+
+const columns: IColumn[] = [
+    {
+        key: 'id',
+        fieldName: 'id',
+        name: 'Id',
+        minWidth: 250
+    },
+    {
+        key: 'sourceStream',
+        fieldName: 'sourceStream',
+        name: 'Source Stream',
+        minWidth: 250
+    },
+    {
+        key: 'position',
+        fieldName: 'position',
+        name: 'Position',
+        minWidth: 100
+    },
+    {
+        key: 'failed',
+        fieldName: 'failed',
+        name: 'Failed',
+        minWidth: 100
+    }
+];
+
+
+export const EventHandlersOverview = () => {
+    const { loading, error, data } = useQuery(query);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <>
+            <DetailsList columns={columns} items={data.eventHandlers.allForTenant} />
+        </>
+    );
+};
