1d406369e243d49fbd2528d26f247dafbbb742ac
Einar Ingebrigtsen <einar@dolittle.com>
2021-06-01 19:55:41 +0200

    Adding test data

--- Source/Management/EventHandlers/EventHandlers.cs
+++ Source/Management/EventHandlers/EventHandlers.cs
@@ -10,7 +10,11 @@ namespace Dolittle.Runtime.Management.EventHandlers
     {
         public IEnumerable<EventHandler> AllForTenant(Guid tenantId)
         {
-            return Array.Empty<EventHandler>();
+            return new EventHandler[] {
+                new () { Id = Guid.Parse("0571cc9f-1c14-4e05-81cd-18a7557e56c0"), SourceStream = Guid.Empty, Position=0, Failed = false },
+                new () { Id = Guid.Parse("c509ff72-6214-46da-b0c6-7c0ee8f90674"), SourceStream = Guid.Empty, Position=0, Failed = false },
+                new () { Id = Guid.Parse("4822545c-add5-4d10-8b57-7993f559e923"), SourceStream = Guid.Empty, Position=0, Failed = false }
+            };
         }
     }
 }
