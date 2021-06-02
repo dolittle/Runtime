894618edca0568c6a14b314f3eb8db3dfbdc0860
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-06-02 19:42:06 +0200

    Specs for WrappedAsyncStreamWriter

---
+++ Specifications/Services/ReverseCall/for_WrappedAsyncStreamWriter/when_pinging/and_another_write_is_currently_blocking.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Dolittle.Runtime.Services.ReverseCalls.given;
+using Dolittle.Services.Contracts;
+using Machine.Specifications;
+using Moq;
+using It = Machine.Specifications.It;
+
+namespace Dolittle.Runtime.Services.ReverseCalls.for_WrappedAsyncStreamWriter.when_pinging
+{
+    public class and_another_write_is_currently_blocking : given.a_wrapped_stream_writer
+    {
+        static a_message another_message;
+
+        Establish context = () =>
+        {
+            another_message = new();
+
+            original_writer
+                .Setup(_ => _.WriteAsync(another_message))
+                .Returns(Task.Delay(Timeout.Infinite));
+        };
+
+        Because of = () =>
+        {
+            _ = wrapped_writer.WriteAsync(another_message);
+            wrapped_writer.MaybeWritePing().GetAwaiter().GetResult();
+        };
+
+        It should_not_write_the_ping_message_to_the_original_stream = () => original_writer.Verify(_ => _.WriteAsync(Moq.It.IsAny<a_message>()), Times.Once);
+    }
+}
