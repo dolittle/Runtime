cf106664f6c7f47dd6c4b567099f54520f90a5a2
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-06-03 08:58:27 +0200

    Start preparing specs for PingedConnection

--- Specifications/Services/ReverseCall/given/all_dependencies.cs
+++ Specifications/Services/ReverseCall/given/all_dependencies.cs
@@ -1,10 +1,14 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading;
+using Grpc.Core;
 using Machine.Specifications;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using It = Moq.It;
 
 namespace Dolittle.Runtime.Services.ReverseCalls.given
 {
@@ -15,15 +19,22 @@ namespace Dolittle.Runtime.Services.ReverseCalls.given
         protected static ILoggerFactory logger_factory;
         protected static ILogger logger;
         protected static IMetricsCollector metrics;
+        protected static ServerCallContext server_call_context;
         protected static CancellationToken cancellation_token;
 
         Establish context = () =>
         {
             request_id = "request-id";
+
             message_converter = new Mock<IConvertReverseCallMessages<a_message, a_message, object, object, object, object>>();
-            logger_factory = Mock.Of<ILoggerFactory>();
-            logger = Mock.Of<ILogger>();
+
+            logger_factory = NullLoggerFactory.Instance;
+            logger = NullLogger.Instance;
+
             metrics = Mock.Of<IMetricsCollector>();
+
+            server_call_context = Mock.Of<ServerCallContext>();
+
             cancellation_token = CancellationToken.None;
         };
     }
