1139830b9914d95ac5666e924827a2435bdc1d77
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-06-04 08:41:17 +0200

    Make it build

--- Specifications/Events.Processing/EventHandlers/for_EventHandler/when_disposing/and_it_fails_registering_event_processor.cs
+++ Specifications/Events.Processing/EventHandlers/for_EventHandler/when_disposing/and_it_fails_registering_event_processor.cs
@@ -4,6 +4,7 @@
 using System.Threading;
 using Dolittle.Runtime.DependencyInversion;
 using Dolittle.Runtime.Events.Processing.Contracts;
+using Dolittle.Runtime.Events.Processing.Filters;
 using Dolittle.Runtime.Events.Processing.Streams;
 using Dolittle.Runtime.Events.Store.Streams;
 using Machine.Specifications;
