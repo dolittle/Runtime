b095341d5b60cc723606b81bbbc58b0ff0b4453b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-06-05 14:19:02 +0200

    Introduce SubscriptionState, and change ISubscription interface.
    It is now supposed to live on forever (retrying) - and should therefore
    never be disposed of. It will just die when the Runtime is stopped.

--- Source/EventHorizon/Consumer/ISubscription.cs
+++ Source/EventHorizon/Consumer/ISubscription.cs
@@ -1,32 +1,41 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Threading.Tasks;
 using Dolittle.Runtime.EventHorizon.Consumer;
-using Dolittle.Runtime.Events.Store.EventHorizon;
 
 namespace Dolittle.Runtime.EventHorizon
 {
     /// <summary>
     /// Defines a resilient subscription to an event horizon.
     /// </summary>
-    public interface ISubscription : IDisposable
+    public interface ISubscription
     {
-        /// <summary>
-        /// Registers the subscription.
-        /// </summary>
-        /// <returns>A <see cref="Task{TResult}" /> that, when resolved, returns a <see cref="SubscriptionResponse" />.</returns>
-        Task<SubscriptionResponse> Register();
-
         /// <summary>
         /// Gets the <see cref="SubscriptionId" />.
         /// </summary>
         SubscriptionId Identifier { get; }
 
         /// <summary>
-        /// Gets the <see cref="ConsentId" />.
+        /// Gets the current state of the subscription.
         /// </summary>
-        ConsentId Consent { get; }
+        SubscriptionState State { get; }
+
+        /// <summary>
+        /// Starts the resislient subscription.
+        /// </summary>
+        /// <remarks>
+        /// This will start a new connection to the producer runtime, and will keep retrying the connection forever if it fails.
+        /// </remarks>
+        void Start();
+
+        /// <summary>
+        /// Gets a task that, when resolved returns the subscription response of the last connection.
+        /// </summary>
+        /// <remarks>
+        /// If the subscription state is <see cref="SubscriptionState.Connected"/>, this will immediately resolve to the last connection response.
+        /// Otherwise, it will resolve once the next connection attempt succeeds or fails.
+        /// </remarks>
+        Task<SubscriptionResponse> ConnectionResponse { get; }
     }
 }
---
+++ Source/EventHorizon/Consumer/SubscriptionState.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Dolittle.Runtime.EventHorizon
+{
+    /// <summary>
+    /// Represents the current state of a <see cref="ISubscription"/>.
+    /// </summary>
+    public enum SubscriptionState
+    {
+        /// <summary>
+        /// This state means that the subscription is newly created, and has not been started yet.
+        /// </summary>
+        Created,
+
+        /// <summary>
+        /// This state means that the subscription is currently connecting to the producer Runtime.
+        /// </summary>
+        Connecting,
+
+        /// <summary>
+        /// This state means that the subscription is currently connected to the producer Runtime and processing events.
+        /// </summary>
+        Connected,
+
+        /// <summary>
+        /// This state means that the last connection to the producer Runtime failed, and that the subscription will restart.
+        /// </summary>
+        FailedToConnect,
+
+        /// <summary>
+        /// This state means that the subscription failed to to process events during the last connection, and that the subscription will restart.
+        /// </summary>
+        FailedToProcess,
+    }
+}
