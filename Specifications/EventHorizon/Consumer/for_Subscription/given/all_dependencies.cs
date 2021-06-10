0a44aded4fd72f35ad1b4aa1ce22ecd8ba6c6a44
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-06-10 11:41:42 +0200

    Add specs to Subscription

--- Specifications/EventHorizon/Consumer/for_Subscription/given/all_dependencies.cs
+++ Specifications/EventHorizon/Consumer/for_Subscription/given/all_dependencies.cs
@@ -6,12 +6,15 @@ using Machine.Specifications;
 using Moq;
 using Dolittle.Runtime.Resilience;
 using Dolittle.Runtime.Microservices;
-using System.Collections.Generic;
-using Dolittle.Runtime.ApplicationModel;
-using System.Linq;
 using Microsoft.Extensions.Logging;
 using Dolittle.Runtime.EventHorizon.Consumer.Connections;
 using Dolittle.Runtime.EventHorizon.Consumer.Processing;
+using System.Threading.Tasks;
+using System.Threading;
+using Dolittle.Runtime.Events.Store.Streams;
+using Dolittle.Runtime.Events.Store.EventHorizon;
+using Nito.AsyncEx;
+using Dolittle.Runtime.Protobuf;
 
 namespace Dolittle.Runtime.EventHorizon.Consumer.for_Subscription.given
 {
@@ -23,10 +26,22 @@ namespace Dolittle.Runtime.EventHorizon.Consumer.for_Subscription.given
         protected static Mock<IStreamProcessorFactory> stream_processor_factory;
         protected static Mock<IGetNextEventToReceiveForSubscription> get_next_event;
         protected static Mock<IAsyncPolicyFor<Subscription>> policy;
+        protected static Mock<IEventHorizonConnection> event_horizon_connection;
+        protected static Mock<IStreamProcessor> stream_processor;
+
+        protected static CancellationTokenSource policy_cts;
+        protected static CancellationTokenSource stream_processor_cts;
+        protected static CancellationTokenSource event_horizon_connection_cts;
+        protected static ConsentId consent;
+        protected static StreamPosition subscription_stream_position;
         protected static Subscription subscription;
 
         Establish context = () =>
         {
+            subscription_stream_position = 5;
+            policy_cts = new CancellationTokenSource();
+            stream_processor_cts = new CancellationTokenSource();
+            event_horizon_connection_cts = new CancellationTokenSource();
             subscription_id = new SubscriptionId(
                 "aa6403de-6dee-4db8-80a0-366bb9532b3a",
                 Guid.Parse("8aef7b88-db5b-4e17-92f7-cfd1cb23a829"),
@@ -41,6 +56,8 @@ namespace Dolittle.Runtime.EventHorizon.Consumer.for_Subscription.given
             get_next_event = new Mock<IGetNextEventToReceiveForSubscription>();
             policy = new Mock<IAsyncPolicyFor<Subscription>>();
 
+            EstablishInitialMockSetups();
+
             subscription = new Subscription(
                 subscription_id,
                 producer_microservice_address,
@@ -50,5 +67,69 @@ namespace Dolittle.Runtime.EventHorizon.Consumer.for_Subscription.given
                 get_next_event.Object,
                 Mock.Of<ILogger>());
         };
+
+        Cleanup clean = () =>
+        {
+            policy_cts.Cancel();
+            policy_cts.Dispose();
+            stream_processor_cts.Cancel();
+            stream_processor_cts.Dispose();
+            event_horizon_connection_cts.Cancel();
+            event_horizon_connection_cts.Dispose();
+        };
+
+        static void EstablishInitialMockSetups()
+        {
+            policy
+                .Setup(_ => _.Execute(Moq.It.IsAny<Func<CancellationToken, Task>>(), Moq.It.IsAny<CancellationToken>()))
+                .Returns<Func<CancellationToken, Task>, CancellationToken>((callback, cancellationToken) => Task.Run(async () =>
+                {
+                    while (!policy_cts.Token.IsCancellationRequested && !cancellationToken.IsCancellationRequested)
+                    {
+                        try
+                        {
+                            await callback(cancellationToken).ConfigureAwait(false);
+                        }
+                        catch
+                        { }
+                        await Task.Delay(10).ConfigureAwait(false);
+                    }
+                }));
+            event_horizon_connection = new Mock<IEventHorizonConnection>();
+            event_horizon_connection_factory
+                .Setup(_ => _.Create(producer_microservice_address))
+                .Returns(event_horizon_connection.Object);
+            event_horizon_connection
+                .Setup(_ => _.Connect(subscription_id, Moq.It.IsAny<StreamPosition>(), Moq.It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(SubscriptionResponse.Succeeded(consent)));
+
+            get_next_event
+                .Setup(_ => _.GetNextEventToReceiveFor(subscription_id, Moq.It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(subscription_stream_position));
+
+            stream_processor = new Mock<IStreamProcessor>();
+            stream_processor_factory
+                .Setup(_ => _.Create(consent, subscription_id, Moq.It.IsAny<EventsFromEventHorizonFetcher>()))
+                .Returns(stream_processor.Object);
+
+            stream_processor
+                .Setup(_ => _.StartAndWait(Moq.It.IsAny<CancellationToken>()))
+                .Returns<CancellationToken>(cancellationToken => Task.Run(async () =>
+                {
+                    while (!stream_processor_cts.Token.IsCancellationRequested && !cancellationToken.IsCancellationRequested)
+                    {
+                        await Task.Delay(50).ConfigureAwait(false);
+                    }
+                }));
+            event_horizon_connection
+                .Setup(_ => _.StartReceivingEventsInto(Moq.It.IsAny<AsyncProducerConsumerQueue<StreamEvent>>(), Moq.It.IsAny<CancellationToken>()))
+                .Returns<AsyncProducerConsumerQueue<StreamEvent>, CancellationToken>((_, cancellationToken) => Task.Run(async () =>
+                {
+                    while (!event_horizon_connection_cts.Token.IsCancellationRequested && !cancellationToken.IsCancellationRequested)
+                    {
+                        await Task.Delay(50).ConfigureAwait(false);
+                    }
+                }));
+        }
     }
 }
---
+++ Specifications/EventHorizon/Consumer/for_Subscription/when_starting/and_connection_succeeds/and_stream_processor_fails.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Dolittle.Runtime.EventHorizon.Consumer.Processing;
+using Dolittle.Runtime.Events.Store.EventHorizon;
+using Dolittle.Runtime.Events.Store.Streams;
+using Dolittle.Runtime.Microservices;
+using Machine.Specifications;
+using Nito.AsyncEx;
+
+namespace Dolittle.Runtime.EventHorizon.Consumer.for_Subscription.when_starting.and_connection_succeeds
+{
+    public class and_stream_processor_fails : given.all_dependencies
+    {
+        Establish context = () =>
+        {
+            stream_processor
+                .Setup(_ => _.StartAndWait(Moq.It.IsAny<CancellationToken>()))
+                .Returns(Task.Run(() => throw new Exception()));
+        };
+        Because of = () =>
+        {
+            subscription.Start();
+            Task.Delay(100).GetAwaiter().GetResult();
+        };
+
+        It should_create_connection_at_least_twice = () => event_horizon_connection_factory.Verify(_ => _.Create(producer_microservice_address), Moq.Times.AtLeast(2));
+        It should_create_stream_processor_at_least_twice = () => stream_processor_factory.Verify(_ => _.Create(
+            consent,
+            subscription_id,
+            Moq.It.IsAny<EventsFromEventHorizonFetcher>()), Moq.Times.AtLeast(2));
+
+        It should_connect_at_least_twice = () => event_horizon_connection.Verify(_ => _.Connect(
+            subscription_id,
+            subscription_stream_position,
+            Moq.It.IsAny<CancellationToken>()), Moq.Times.AtLeast(2));
+
+        It should_start_stream_processor_at_least_twice = () => stream_processor.Verify(_ => _.StartAndWait(
+            Moq.It.IsAny<CancellationToken>()), Moq.Times.AtLeast(2));
+
+        It should_start_receiving_events_at_least_twice = () => event_horizon_connection.Verify(_ => _.StartReceivingEventsInto(
+            Moq.It.IsAny<AsyncProducerConsumerQueue<StreamEvent>>(),
+            Moq.It.IsAny<CancellationToken>()), Moq.Times.AtLeast(2));
+
+        It should_be_connected = () => subscription.State.ShouldEqual(SubscriptionState.FailedToProcess);
+        It should_get_the_successfull_response = () => subscription.ConnectionResponse.Result.Success.ShouldBeTrue();
+        It should_have_the_correct_consent = () => subscription.ConnectionResponse.Result.ConsentId.ShouldEqual(consent);
+    }
+}
