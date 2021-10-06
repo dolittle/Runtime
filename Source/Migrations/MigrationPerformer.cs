da2da3e9962b407c4ba4359da5a604a0daf691f4
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-10-06 13:21:30 +0200

    Implement PerformForTenant on MigrationPerformer

--- Source/Migrations/MigrationPerformer.cs
+++ Source/Migrations/MigrationPerformer.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading.Tasks;
 using Dolittle.Runtime.ApplicationModel;
+using Dolittle.Runtime.Events.Store.MongoDB;
 using Dolittle.Runtime.Events.Store.MongoDB.Migrations;
 using Dolittle.Runtime.ResourceTypes.Configuration;
 using Dolittle.Runtime.Rudimentary;
@@ -20,17 +22,41 @@ namespace Dolittle.Runtime.Migrations
         {
             _resources = resources;
         }
-        
+
         /// <inheritdoc/>
         public Task<Try> PerformForTenant(ICanMigrateAnEventStore eventStoreMigration, TenantId tenant)
+        {
+            var configuration = GetConfiguration(tenant);
+            if (!configuration.Success)
+            {
+                return Task.FromResult(Try.Failed(configuration.Exception));
+            }
+            return eventStoreMigration.Migrate(configuration.Result);
+        }
+
+            /// <inheritdoc/>
+        public Task<Try> PerformForAllTenants(ICanMigrateAnEventStore eventStoreMigration)
         {
             return Task.FromResult(Try.Succeeded());
         }
 
-        /// <inheritdoc/>
-        public Task<Try> PerformForAllTenants(ICanMigrateAnEventStore eventStoreMigration)
+        Try<EventStoreConfiguration> GetConfiguration(TenantId tenant)
         {
-            return Task.FromResult(Try.Succeeded());
+            try
+            {
+                if (!_resources.ContainsKey(tenant))
+                {
+                    return new TenantNotConfigured(tenant);
+                }
+                var resources = _resources[tenant];
+                return !resources.ContainsKey("eventStore") 
+                    ? new EventStoreNotConfiguredForTenant(tenant)
+                    : Try<EventStoreConfiguration>.Succeeded(resources["eventStore"] as EventStoreConfiguration);
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
         }
     }
 }
