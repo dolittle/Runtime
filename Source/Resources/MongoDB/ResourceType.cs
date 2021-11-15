65faf816e54edfff8b154ac9a95fd50ff96795bb
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-11-15 15:20:52 +0100

    Make remark to remind ourselves of why the MongoDB resource type is called "readModels"

--- Source/Resources/MongoDB/ResourceType.cs
+++ Source/Resources/MongoDB/ResourceType.cs
@@ -8,7 +8,7 @@ using Dolittle.Runtime.ResourceTypes;
 namespace Dolittle.Runtime.Resources.MongoDB
 {
     /// <summary>
-    /// Represents a <see cref="IAmAResourceType">resource type</see> for the Mongo Db resource.
+    /// Represents a <see cref="IAmAResourceType">resource type</see> for the MongoDB resource.
     /// </summary>
     /// <remarks>The name is currently readModels to support for legacy cases.</remarks>
     public class ResourceType : IAmAResourceType
@@ -16,6 +16,11 @@ namespace Dolittle.Runtime.Resources.MongoDB
         /// <summary>
         /// The <see cref="ResourceTypes.ResourceType"/> name.
         /// </summary>
+        /// <remarks>
+        /// Although this resource type is a MongoDB specific type that cannot be swapped out with another implementation
+        /// like the historical "Read Models" storage could, we're keeping the implementation for the configuration the
+        /// same to make the Runtime compatible with the previous configuration files style.
+        /// </remarks>
         public static ResourceTypes.ResourceType ResourceTypeName => "readModels";
         
         /// <inheritdoc/>
