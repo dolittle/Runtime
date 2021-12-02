31f03cf88ac7170214ec9a3470e05155e01cae20
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-12-02 10:00:26 +0100

    Built in version info

---
+++ Source/Server/VersionInfo.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Dolittle.Runtime.Versioning;
+
+namespace Dolittle.Runtime.Server
+{
+    /// <summary>
+    /// Represents a container for the Dolittle Runtime version.
+    /// </summary>
+    public static class VersionInfo
+    {
+        /// <summary>
+        /// Gets the current <see cref="Version"/> of the Dolittle Runtime.
+        /// </summary>
+        public static Version CurrentVersion => new(377, 389, 368, 0, "PRERELEASE");
+    }
+    
+}
+
