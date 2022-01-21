4bf1e67157568009b3e6299f55134bae4dbf2ac1
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-01-21 23:18:34 +0100

    ReImplement MongoDB ProjectionStates to use async enumerable

--- Source/Projections.Store.MongoDB/State/ProjectionStates.cs
+++ Source/Projections.Store.MongoDB/State/ProjectionStates.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Dolittle.Runtime.Events.Store;
 using Dolittle.Runtime.Lifecycle;
+using Dolittle.Runtime.MongoDB;
 using Dolittle.Runtime.Projections.Store.State;
 using Dolittle.Runtime.Rudimentary;
 using MongoDB.Bson;
@@ -37,16 +38,9 @@ public class ProjectionStates : IProjectionStates
     {
         try
         {
-            await OnProjection(
-                projection,
-                scope,
-                async collection =>
-                {
-                    var deleteResult = await collection.DeleteManyAsync(Builders<Projection>.Filter.Empty, token).ConfigureAwait(false);
-                    return deleteResult.IsAcknowledged;
-                },
-                token).ConfigureAwait(false);
-            return true;
+            var collection = await _projections.GetStates(scope, projection, token).ConfigureAwait(false);
+            var deleteResult = await collection.DeleteManyAsync(Builders<Projection>.Filter.Empty, token).ConfigureAwait(false);
+            return deleteResult.IsAcknowledged;
         }
         catch (MongoWaitQueueFullException)
         {
@@ -59,14 +53,13 @@ public class ProjectionStates : IProjectionStates
     {
         try
         {
-            var projectionState = await OnProjection(
-                projection,
-                scope,
-                async collection => await collection
-                    .Find(CreateKeyFilter(key))
-                    .Project(_ => _.ContentRaw)
-                    .SingleOrDefaultAsync(token),
-                token).ConfigureAwait(false);
+            var collection = await _projections.GetStates(scope, projection, token).ConfigureAwait(false);
+            var projectionState = await collection
+                .Find(CreateKeyFilter(key))
+                .Project(_ => _.ContentRaw)
+                .SingleOrDefaultAsync(token)
+                .ConfigureAwait(false);
+            
             return string.IsNullOrEmpty(projectionState)
                 ? Try<ProjectionState>.Failed(new ProjectionStateDoesNotExist(projection, key, scope))
                 : new ProjectionState(projectionState);
@@ -78,24 +71,22 @@ public class ProjectionStates : IProjectionStates
     }
 
     /// <inheritdoc/>
-    public async Task<Try<IEnumerable<(ProjectionState State, ProjectionKey Key)>>> TryGetAll(ProjectionId projection, ScopeId scope, CancellationToken token)
+    public async Task<Try<IAsyncEnumerable<(ProjectionState State, ProjectionKey Key)>>> TryGetAll(ProjectionId projection, ScopeId scope, CancellationToken token)
     {
         try
         {
-            var states = await OnProjection(
-                projection,
-                scope,
-                async collection => await collection
-                    .Find(Builders<Projection>.Filter.Empty)
-                    .Project(_ => new Tuple<ProjectionState, ProjectionKey>(_.ContentRaw, _.Key))
-                    .ToListAsync(token),
-                token).ConfigureAwait(false);
+            var collection = await _projections.GetStates(scope, projection, token).ConfigureAwait(false);
+            var states = collection
+                .Find(Builders<Projection>.Filter.Empty)
+                .Project(_ => new Tuple<ProjectionState, ProjectionKey>(_.ContentRaw, _.Key))
+                .ToAsyncEnumerable(token);
+            
             var result = states.Select(_ =>
             {
                 return (_.Item1, _.Item2);
             });
 
-            return Try<IEnumerable<(ProjectionState State, ProjectionKey Key)>>.Succeeded(result);
+            return Try<IAsyncEnumerable<(ProjectionState State, ProjectionKey Key)>>.Succeeded(result);
         }
         catch (Exception ex)
         {
@@ -108,17 +99,9 @@ public class ProjectionStates : IProjectionStates
     {
         try
         {
-            return await OnProjection(
-                projection,
-                scope,
-                async collection =>
-                {
-                    var deleteResult = await collection.DeleteOneAsync(
-                        CreateKeyFilter(key),
-                        token).ConfigureAwait(false);
-                    return deleteResult.IsAcknowledged;
-                },
-                token).ConfigureAwait(false);
+            var collection = await _projections.GetStates(scope, projection, token).ConfigureAwait(false);
+            var deleteResult = await collection.DeleteOneAsync(CreateKeyFilter(key), token).ConfigureAwait(false);
+            return deleteResult.IsAcknowledged;
         }
         catch (MongoWaitQueueFullException)
         {
@@ -131,33 +114,24 @@ public class ProjectionStates : IProjectionStates
     {
         try
         {
-            return await OnProjection(
-                projection,
-                scope,
-                async collection =>
-                {
-                    var filter = CreateKeyFilter(key);
-                    var updateDefinition = Builders<Projection>
-                        .Update
-                        .Set(_ => _.Content, BsonDocument.Parse(state.Value))
-                        .Set(_ => _.ContentRaw, state.Value);
-                    var updateResult = await collection.UpdateOneAsync(
-                        filter,
-                        updateDefinition,
-                        new UpdateOptions { IsUpsert = true },
-                        token).ConfigureAwait(false);
-                    return updateResult.IsAcknowledged;
-                },
+            var collection = await _projections.GetStates(scope, projection, token).ConfigureAwait(false);
+            var filter = CreateKeyFilter(key);
+            var updateDefinition = Builders<Projection>
+                .Update
+                .Set(_ => _.Content, BsonDocument.Parse(state.Value))
+                .Set(_ => _.ContentRaw, state.Value);
+            var updateResult = await collection.UpdateOneAsync(
+                filter,
+                updateDefinition,
+                new UpdateOptions { IsUpsert = true },
                 token).ConfigureAwait(false);
+            return updateResult.IsAcknowledged;
         }
         catch (MongoWaitQueueFullException)
         {
             return false;
         }
     }
-
-    async Task<TResult> OnProjection<TResult>(ProjectionId projection, ScopeId scope, Func<IMongoCollection<Projection>, Task<TResult>> callback, CancellationToken token)
-        => await callback(await _projections.GetStates(scope, projection, token).ConfigureAwait(false)).ConfigureAwait(false);
 
     FilterDefinition<Projection> CreateKeyFilter(ProjectionKey key) => Builders<Projection>.Filter.Eq(_ => _.Key, key.Value);
 }
