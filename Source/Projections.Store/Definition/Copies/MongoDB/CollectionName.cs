4d2107616ef8d74a3fbd4e8c9ae2a16d352b175f
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-01-26 16:08:26 +0100

    Validate MongoDB collection name concept and add specs.

--- Source/Projections.Store/Definition/Copies/MongoDB/CollectionName.cs
+++ Source/Projections.Store/Definition/Copies/MongoDB/CollectionName.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Text;
 using Dolittle.Runtime.Rudimentary;
 
 namespace Dolittle.Runtime.Projections.Store.Definition.Copies.MongoDB;
@@ -8,9 +10,21 @@ namespace Dolittle.Runtime.Projections.Store.Definition.Copies.MongoDB;
 /// <summary>
 /// Represents the name of the collection to store MongoDB read model copies in.
 /// </summary>
-/// <param name="Value">The name of the collection.</param>
-public record CollectionName(string Value) : ConceptAs<string>(Value)
+public record CollectionName : ConceptAs<string>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CollectionName"/> record.
+    /// </summary>
+    /// <param name="name">The collection name.</param>
+    public CollectionName(string name)
+        : base(name)
+    {
+        ThrowIfNameIsEmpty(name);
+        ThrowIfNameIsTooLong(name);
+        ThrowIfNameContainsIllegalCharacter(name);
+        ThrowIfNameBeginsWithSystemPrefix(name);
+    }
+
     /// <summary>
     /// Gets the <see cref="CollectionName"/> value when it is not set.
     /// </summary>
@@ -22,4 +36,40 @@ public record CollectionName(string Value) : ConceptAs<string>(Value)
     /// <param name="name">The <see cref="string"/> representation of the collection name.</param>
     /// <returns>The converted <see cref="CollectionName"/> concept.</returns>
     public static implicit operator CollectionName(string name) => new(name);
+
+    static void ThrowIfNameIsEmpty(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new InvalidMongoDBCollectionName(name, "it cannot be empty");
+        }
+    }
+
+    static void ThrowIfNameIsTooLong(string name)
+    {
+        if (Encoding.UTF8.GetByteCount(name) >= 120)
+        {
+            throw new InvalidMongoDBCollectionName(name, "it must be at most 120 bytes long");
+        }
+    }
+    
+    static void ThrowIfNameContainsIllegalCharacter(string name)
+    {
+        if (name.Contains('$'))
+        {
+            throw new InvalidMongoDBCollectionName(name, "it cannot contain the character '$'");
+        }
+        if (name.Contains('\0'))
+        {
+            throw new InvalidMongoDBCollectionName(name, "it cannot contain the null character");
+        }
+    }
+
+    static void ThrowIfNameBeginsWithSystemPrefix(string name)
+    {
+        if (name.StartsWith("system.", StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new InvalidMongoDBCollectionName(name, "it cannot start with 'system.'");
+        }
+    }
 }
---
+++ Specifications/Projections.Store/Definitions/Copies/MongoDB/for_CollectionName/when_constructing/and_name_contains_dollar_character.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Machine.Specifications;
+
+namespace Dolittle.Runtime.Projections.Store.Definition.Copies.MongoDB.for_CollectionName.when_constructing;
+
+public class and_name_contains_dollar_character
+{
+    static Exception exception;
+
+    Because of = () => exception = Catch.Exception(() => new CollectionName("almost_legal_name_$"));
+
+    It should_fail = () => exception.ShouldBeOfExactType<InvalidMongoDBCollectionName>();
+}
