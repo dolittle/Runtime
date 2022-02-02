616cecb200dd96465e7f05083f39ce2fda7325c4
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-02 09:38:51 +0100

    Start creating specs for ProjectionConverter

---
+++ Specifications/Projections.Store.Copies.MongoDB/for_ProjectionConverter/when_converting/without_conversions/a_simple_state.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Machine.Specifications;
+using MongoDB.Bson;
+
+namespace Dolittle.Runtime.Projections.Store.Copies.MongoDB.for_ProjectionConverter.when_converting.without_conversions;
+
+public class a_simple_state : given.a_converter_and_inputs
+{
+    Establish context = () => state_to_convert = @"
+        {
+            ""some_string"": ""hello world"",
+            ""some_int"": 42,
+            ""some_bool"": true,
+            ""some_date"": ""2002-02-02T02:02:02.002Z""
+        }
+    ";
+
+    static BsonDocument result;
+
+    Because of = () => result = projection_converter.Convert(state_to_convert, conversions_to_apply);
+
+    It should_have_the_correct_string = () => result["some_string"].ShouldEqual(new BsonString("hello world"));
+    It should_have_the_correct_int = () => result["some_int"].ShouldEqual(new BsonInt32(42));
+    It should_have_the_correct_bool = () => result["some_bool"].ShouldEqual(new BsonBoolean(true));
+    It should_have_the_correct_date = () => result["some_date"].ShouldEqual(new BsonString("2002-02-02T02:02:02.002Z"));
+}
