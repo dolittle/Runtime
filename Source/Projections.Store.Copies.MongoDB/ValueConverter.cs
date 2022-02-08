aa576cd64ff5243faea7a66a883825503c3420c4
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-08 11:23:41 +0100

    Implement new conversions in ValueConverter

--- Source/Projections.Store.Copies.MongoDB/ValueConverter.cs
+++ Source/Projections.Store.Copies.MongoDB/ValueConverter.cs
@@ -5,8 +5,11 @@ using System;
 using System.Globalization;
 using Dolittle.Runtime.Projections.Store.Definition.Copies.MongoDB;
 using MongoDB.Bson;
+using MongoDB.Bson.IO;
 
 namespace Dolittle.Runtime.Projections.Store.Copies.MongoDB;
+
+#pragma warning disable CS8509
 
 /// <summary>
 /// Represents an implementation of <see cref="IValueConverter"/>.
@@ -15,25 +18,75 @@ public class ValueConverter : IValueConverter
 {
     /// <inheritdoc />
     public BsonValue Convert(BsonValue value, ConversionBSONType conversion)
+    {
+        switch (conversion)
+        {
+            case ConversionBSONType.None:
+                return value;
+            case ConversionBSONType.DateAsDate:
+            case ConversionBSONType.DateAsArray:
+            case ConversionBSONType.DateAsDocument:
+            case ConversionBSONType.DateAsString:
+            case ConversionBSONType.DateAsInt64:
+                return ConvertToDate(value, conversion);
+            case ConversionBSONType.GuidAsStandardBinary:
+            case ConversionBSONType.GuidAsCsharpLegacyBinary:
+            case ConversionBSONType.GuidAsString:
+                return ConvertToGuid(value, conversion);
+            default:
+                throw new UnknownBSONConversionType(conversion);
+        }
+    }
+
+    static BsonValue ConvertToDate(BsonValue value, ConversionBSONType conversion)
+    {
+        if (value is BsonDateTime dateTimeValue)
+        {
+            return ConvertDateTo(new DateTimeOffset(dateTimeValue.ToUniversalTime(), TimeSpan.Zero), conversion);
+        }
+
+        if (value is BsonString stringValue)
+        {
+            return ConvertDateTo(DateTimeOffset.Parse(stringValue.Value, CultureInfo.InvariantCulture), conversion);
+        }
+
+        throw new CannotConvertValueUsingConversion(value, conversion);
+    }
+
+    static BsonValue ConvertDateTo(DateTimeOffset value, ConversionBSONType conversion)
         => conversion switch
         {
-            ConversionBSONType.None => value,
-            ConversionBSONType.DateAsDate => ConvertToDateAsDate(value),
-            ConversionBSONType.GuidAsStandardBinary => ConvertToGuidAsStandardBinary(value),
-            _ => throw new UnknownBSONConversionType(conversion),
+            ConversionBSONType.DateAsDate => new BsonDateTime(value.UtcDateTime),
+            ConversionBSONType.DateAsArray => new BsonArray
+            {
+                new BsonInt64(value.Ticks),
+                new BsonInt32((int) value.Offset.TotalMinutes),
+            },
+            ConversionBSONType.DateAsDocument => new BsonDocument
+            {
+                new BsonElement("DateTime", BsonUtils.ToMillisecondsSinceEpoch(value.UtcDateTime)),
+                new BsonElement("Ticks", new BsonInt64(value.Ticks)),
+                new BsonElement("Offset", new BsonInt32((int) value.Offset.TotalMinutes)),
+            },
+            ConversionBSONType.DateAsString => JsonConvert.ToString(value),
+            ConversionBSONType.DateAsInt64 => new BsonInt64(value.Ticks),
         };
 
-    static BsonDateTime ConvertToDateAsDate(BsonValue value)
-        => value switch
+    static BsonValue ConvertToGuid(BsonValue value, ConversionBSONType conversion)
+    {
+        if (value is BsonString stringValue)
         {
-            BsonDateTime dateTimeValue => dateTimeValue,
-            BsonString stringValue => new BsonDateTime(DateTimeOffset.Parse(stringValue.Value, CultureInfo.InvariantCulture).UtcDateTime),
-            _ => throw new CannotConvertValueUsingConversion(value, ConversionBSONType.DateAsDate),
-        };
+            return ConvertGuidTo(Guid.Parse(stringValue.Value), conversion);
+        }
+        
+        throw new CannotConvertValueUsingConversion(value, conversion);
+    }
 
-    static BsonBinaryData ConvertToGuidAsStandardBinary(BsonValue value)
-        => value switch
+    static BsonValue ConvertGuidTo(Guid value, ConversionBSONType conversion)
+        => conversion switch
         {
-            _ => throw new CannotConvertValueUsingConversion(value, ConversionBSONType.GuidAsStandardBinary),
+            ConversionBSONType.GuidAsStandardBinary => new BsonBinaryData(value, GuidRepresentation.Standard),
+            ConversionBSONType.GuidAsCsharpLegacyBinary => new BsonBinaryData(value, GuidRepresentation.CSharpLegacy),
+            ConversionBSONType.GuidAsString => new BsonString(value.ToString()),
         };
 }
