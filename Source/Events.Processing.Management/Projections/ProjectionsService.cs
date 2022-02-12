f563db8fe13891d36b488ab6bebaf68a93d35c60
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-12 12:54:02 +0100

    Handle exceptions nicely in Projections managment service.

--- Source/Events.Processing.Management/Projections/ProjectionsService.cs
+++ Source/Events.Processing.Management/Projections/ProjectionsService.cs
@@ -1,32 +1,39 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dolittle.Protobuf.Contracts;
 using Dolittle.Runtime.ApplicationModel;
 using Dolittle.Runtime.Events.Processing.Management.Contracts;
 using Dolittle.Runtime.Events.Processing.Management.StreamProcessors;
 using Dolittle.Runtime.Events.Processing.Projections;
+using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Projections.Store;
 using Dolittle.Runtime.Protobuf;
+using Dolittle.Runtime.Rudimentary;
 using Grpc.Core;
 using static Dolittle.Runtime.Events.Processing.Management.Contracts.Projections;
-using Failure = Dolittle.Protobuf.Contracts.Failure;
 
 namespace Dolittle.Runtime.Events.Processing.Management.Projections;
 
 public class ProjectionsService : ProjectionsBase
 {
     readonly IProjections _projections;
+    readonly IExceptionToFailureConverter _exceptionToFailureConverter;
     readonly IConvertProjectionDefinitions _definitionConverter;
     readonly IConvertStreamProcessorStatuses _streamProcessorStatusConverter;
 
     public ProjectionsService(
         IProjections projections,
+        IExceptionToFailureConverter exceptionToFailureConverter,
         IConvertProjectionDefinitions definitionConverter,
         IConvertStreamProcessorStatuses streamProcessorStatusConverter)
     {
         _projections = projections;
+        _exceptionToFailureConverter = exceptionToFailureConverter;
         _definitionConverter = definitionConverter;
         _streamProcessorStatusConverter = streamProcessorStatusConverter;
     }
@@ -42,48 +49,49 @@ public class ProjectionsService : ProjectionsBase
     /// <inheritdoc />
     public override Task<GetOneProjectionResponse> GetOne(GetOneProjectionRequest request, ServerCallContext context)
     {
-        var info = _projections.All.FirstOrDefault(projection =>
-            projection.Definition.Scope.Value == request.ScopeId.ToGuid() &&
-            projection.Definition.Projection.Value == request.ProjectionId.ToGuid());
-
         var response = new GetOneProjectionResponse();
         
-        if (info == default)
+        var getIds = GetScopeAndProjectionIds(request.ScopeId, request.ProjectionId, out var scope, out var projection);
+        if (!getIds.Success)
         {
-            response.Failure = new Failure
-            {
-                Id = FailureId.Other.ToProtobuf(),
-                Reason = $"Projection {request.ProjectionId.ToGuid()} in scope {request.ScopeId.ToGuid()} is not registered",
-            };
-        }
-        else
-        {
-            response.Projection = CreateStatusFromInfo(info, request.TenantId?.ToGuid());
+            response.Failure = _exceptionToFailureConverter.ToFailure(getIds.Exception);
+            return Task.FromResult(response);
         }
 
+        var info = _projections.All.FirstOrDefault(_ => _.Definition.Scope == scope && _.Definition.Projection == projection);
+        if (info == default)
+        {
+            response.Failure = _exceptionToFailureConverter.ToFailure(new ProjectionNotRegistered(request.ScopeId.ToGuid(), request.ProjectionId.ToGuid()));
+            return Task.FromResult(response);
+        }
+        
+        response.Projection = CreateStatusFromInfo(info, request.TenantId?.ToGuid());
         return Task.FromResult(response);
     }
 
     /// <inheritdoc />
     public override async Task<ReplayProjectionResponse> Replay(ReplayProjectionRequest request, ServerCallContext context)
     {
-        var result = request.TenantId == null
-            ? await _projections.ReplayEventsForAllTenants(request.ScopeId.ToGuid(), request.ProjectionId.ToGuid()).ConfigureAwait(false)
-            : await _projections.ReplayEventsForTenant(request.ScopeId.ToGuid(), request.ProjectionId.ToGuid(), request.TenantId.ToGuid()).ConfigureAwait(false);
-
         var response = new ReplayProjectionResponse();
+        
+        var getIds = GetScopeAndProjectionIds(request.ScopeId, request.ProjectionId, out var scope, out var projection);
+        if (!getIds.Success)
+        {
+            response.Failure = _exceptionToFailureConverter.ToFailure(getIds.Exception);
+            return response;
+        }
+        
+        var result = request.TenantId == null
+            ? await _projections.ReplayEventsForAllTenants(scope, projection).ConfigureAwait(false)
+            : await _projections.ReplayEventsForTenant(scope, projection, request.TenantId.ToGuid()).ConfigureAwait(false);
+
         if (!result.Success)
         {
-            response.Failure = new Failure
-            {
-                Id = FailureId.Other.ToProtobuf(),
-                Reason = result.Exception.Message,
-            };
+            response.Failure = _exceptionToFailureConverter.ToFailure(result.Exception);
         }
 
         return response;
     }
-
 
     ProjectionStatus CreateStatusFromInfo(ProjectionInfo info, TenantId tenant = null)
     {
@@ -111,5 +119,25 @@ public class ProjectionsService : ProjectionsBase
         return tenant == null
             ? _streamProcessorStatusConverter.Convert(state.Result)
             : _streamProcessorStatusConverter.ConvertForTenant(state.Result, tenant);
+    }
+
+    static Try GetScopeAndProjectionIds(Uuid scope, Uuid projection, out ScopeId scopeId, out ProjectionId projectionId)
+    {
+        scopeId = default;
+        projectionId = default;
+        
+        if (scope == default)
+        {
+            return Try.Failed(new ArgumentNullException(nameof(scope), "Scope id is missing in request"));
+        }
+        if (projection == default)
+        {
+            return Try.Failed(new ArgumentNullException(nameof(projection), "Projection id is missing in request"));
+        }
+        
+        scopeId = scope.ToGuid();
+        projectionId = projection.ToGuid();
+
+        return Try.Succeeded();
     }
 }
