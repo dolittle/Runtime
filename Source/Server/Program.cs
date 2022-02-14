6265e315ad2909e1ab3f1adbbf19d00001611618
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-14 17:34:21 +0100

    Do some more tests with multiple hosts, turns out - it doesn't work.

--- Source/Server/Program.cs
+++ Source/Server/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Net;
 using Autofac.Extensions.DependencyInjection;
 using Dolittle.Runtime.Configuration.Legacy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var services = new AutofacServiceProviderFactory();
@@ -14,6 +18,34 @@ var host = Host.CreateDefaultBuilder(args)
     {
         configuration.AddLegacyDolittleFiles();
         configuration.AddJsonFile("appsettings.json");
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddRouting();
+    })
+    .ConfigureWebHost(_ =>
+    {
+        _.UseKestrel(_ => _.Listen(IPAddress.Any, 50052));
+        _.Configure(_ =>
+        {
+            _.UseRouting();
+            _.UseEndpoints(_ =>
+            {
+                _.MapGet("/", () => "Hello from first host!");
+            });
+        });
+    })
+    .ConfigureWebHost(_ =>
+    {
+        _.UseKestrel(_ => _.Listen(IPAddress.Any, 50053));
+        _.Configure(_ =>
+        {
+            _.UseRouting();
+            _.UseEndpoints(_ =>
+            {
+                _.MapGet("/", () => "Hello from second host!");
+            });
+        });
     })
     .Build();
 
