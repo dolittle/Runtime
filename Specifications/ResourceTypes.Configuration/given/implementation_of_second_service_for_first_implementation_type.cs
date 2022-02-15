0377ae188075c7f414f49c7f09539c557c0d0e80
woksin <sindrewilting@gmail.com>
2022-02-15 20:02:00 +0100

    More unneeded files

--- Specifications/ResourceTypes.Configuration/given/implementation_of_second_service_for_first_implementation_type.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Dolittle.Runtime.ResourceTypes.Configuration.Specs.given;
-
-public class implementation_of_second_service_for_first_implementation_type : second_service
-{
-}
