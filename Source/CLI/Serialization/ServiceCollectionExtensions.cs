25990838b6b0240a738a997e2e7035b156b5017b
woksin <sindrewilting@gmail.com>
2022-02-16 19:00:13 +0100

    Remove Serialization.Json project

--- Source/CLI/Serialization/ServiceCollectionExtensions.cs
+++ Source/CLI/Serialization/ServiceCollectionExtensions.cs
@@ -1,23 +1,24 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using Dolittle.Runtime.Serialization.Json;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Dolittle.Runtime.CLI.Serialization;
-
-public static class ServiceCollectionExtensions
-{
-    /// <summary>
-    /// Adds the services related to serialization to the provided service collection.
-    /// </summary>
-    /// <param name="services">The service collection to add serialization services to.</param>
-    public static void AddSerializers(this ServiceCollection services)
-    {
-        var converters = new StaticConverterProvider(
-            new ConceptConverter());
-        var providers = new StaticConverterProviders(converters);
-        var serializer = new Serializer(providers);
-        services.AddSingleton<ISerializer>(serializer);
-    }
-}
+// // Copyright (c) Dolittle. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// using Dolittle.Runtime.Serialization.Json;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace Dolittle.Runtime.CLI.Serialization;
+//
+// TODO: Redo this
+// public static class ServiceCollectionExtensions
+// {
+//     /// <summary>
+//     /// Adds the services related to serialization to the provided service collection.
+//     /// </summary>
+//     /// <param name="services">The service collection to add serialization services to.</param>
+//     public static void AddSerializers(this ServiceCollection services)
+//     {
+//         var converters = new StaticConverterProvider(
+//             new ConceptConverter());
+//         var providers = new StaticConverterProviders(converters);
+//         var serializer = new Serializer(providers);
+//         services.AddSingleton<ISerializer>(serializer);
+//     }
+// }
--- Specifications/Serialization.Json/for_Serializer/ClassToSerialize.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Dolittle.Runtime.Serialization.Json.Specs.for_Serializer;
-
-public class ClassToSerialize
-{
-    public ISomething Something { get; set; }
-}
