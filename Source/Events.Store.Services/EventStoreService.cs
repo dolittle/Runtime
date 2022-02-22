c871b11bc16780d8e060c6686dae0a4ba5a899c0
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-22 11:04:47 +0100

    Add a bunch of Try excension methods to chain calls together, and use it to clean up the EventStoreService

--- Source/Events.Store.Services/EventStoreService.cs
+++ Source/Events.Store.Services/EventStoreService.cs
@@ -6,11 +6,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Dolittle.Runtime.ApplicationModel;
 using Dolittle.Runtime.Artifacts;
-using Dolittle.Runtime.DependencyInversion;
 using Dolittle.Runtime.DependencyInversion.Lifecycle;
 using Dolittle.Runtime.Execution;
 using Dolittle.Runtime.Rudimentary;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ExecutionContext = Dolittle.Runtime.Execution.ExecutionContext;
 
@@ -43,91 +41,47 @@ public class EventStoreService : IEventStoreService
     }
 
     /// <inheritdoc/>
-    public async Task<Try<CommittedEvents>> TryCommit(UncommittedEvents events, ExecutionContext context, CancellationToken token)
+    public Task<Try<CommittedEvents>> TryCommit(UncommittedEvents events, ExecutionContext context, CancellationToken token)
     {
         _logger.EventsReceivedForCommitting(false, events.Count);
-        
 
-        var commitEvents = await TryPerformInExecutionContext(
-                context, 
-                (eventStore, executionContext) => eventStore.CommitEvents(events, executionContext, token))
-            .ConfigureAwait(false);
-
-        if (commitEvents.Success)
-        {
-            Log.EventsSuccessfullyCommitted(_logger);
-        }
-        else
-        {
-            Log.ErrorCommittingEvents(_logger, commitEvents.Exception);
-        }
-
-        return commitEvents;
+        return _executionContextCreator
+            .TryCreateUsing(context)
+            .Then(executionContext =>
+                _getEventStore(executionContext.Tenant).CommitEvents(events, executionContext, token))
+            .Then(_ => 
+                Log.EventsSuccessfullyCommitted(_logger))
+            .Catch(exception => 
+                Log.ErrorCommittingEvents(_logger, exception));
     }
 
     /// <inheritdoc/>
-    public async Task<Try<CommittedAggregateEvents>> TryCommitForAggregate(UncommittedAggregateEvents events, ExecutionContext context, CancellationToken token)
+    public Task<Try<CommittedAggregateEvents>> TryCommitForAggregate(UncommittedAggregateEvents events, ExecutionContext context, CancellationToken token)
     {
         _logger.EventsReceivedForCommitting(true, events.Count);
-        
-        var commitEvents = await TryPerformInExecutionContext(
-                context, 
-                (eventStore, executionContext) => eventStore.CommitAggregateEvents(events, executionContext, token))
-            .ConfigureAwait(false);
-        
-        if (commitEvents.Success)
-        {
-            Log.AggregateEventsSuccessfullyCommitted(_logger);
-        }
-        else
-        {
-            Log.ErrorCommittingAggregateEvents(_logger, commitEvents.Exception);
-        }
 
-        return commitEvents;
+        return _executionContextCreator
+            .TryCreateUsing(context)
+            .Then(executionContext =>
+                _getEventStore(executionContext.Tenant).CommitAggregateEvents(events, executionContext, token))
+            .Then(_ =>
+                Log.AggregateEventsSuccessfullyCommitted(_logger))
+            .Catch(exception =>
+                Log.ErrorCommittingAggregateEvents(_logger, exception));
     }
 
     /// <inheritdoc/>
-    public async Task<Try<CommittedAggregateEvents>> TryFetchForAggregate(ArtifactId aggregateRoot, EventSourceId eventSource, ExecutionContext context, CancellationToken token)
+    public Task<Try<CommittedAggregateEvents>> TryFetchForAggregate(ArtifactId aggregateRoot, EventSourceId eventSource, ExecutionContext context, CancellationToken token)
     {
         Log.FetchEventsForAggregate(_logger);
 
-        var fetchEvents = await TryPerformInExecutionContext(
-                context, 
-                (eventStore, _) => eventStore.FetchForAggregate(eventSource, aggregateRoot, token))
-            .ConfigureAwait(false);
-
-        if (fetchEvents.Success)
-        {
-            Log.SuccessfullyFetchedEventsForAggregate(_logger);
-        }
-        else
-        {
-            Log.ErrorFetchingEventsFromAggregate(_logger, fetchEvents.Exception);
-        }
-
-        return fetchEvents;
-    }
-
-    async Task<Try<T>> TryPerformInExecutionContext<T>(ExecutionContext requested, Func<IEventStore, ExecutionContext, Task<T>> action)
-    {
-        // TODO: This isn't really all that nice, can we move something out another place or make an extension method?
-        var createExecutionContext = _executionContextCreator.TryCreateUsing(requested);
-        if (!createExecutionContext.Success)
-        {
-            return createExecutionContext.Exception;
-        }
-
-        var executionContext = createExecutionContext.Result;
-
-        try
-        {
-            var eventStore = _getEventStore(executionContext.Tenant);
-            return await action(eventStore, executionContext).ConfigureAwait(false);
-        }
-        catch (Exception exception)
-        {
-            return exception;
-        }
+        return _executionContextCreator
+            .TryCreateUsing(context)
+            .Then(executionContext =>
+                _getEventStore(executionContext.Tenant).FetchForAggregate(eventSource, aggregateRoot, token))
+            .Then(_ =>
+                Log.SuccessfullyFetchedEventsForAggregate(_logger))
+            .Catch(exception =>
+                Log.ErrorFetchingEventsFromAggregate(_logger, exception));
     }
 }
