0449b4470857e9ce65ce6bffe4e8fc73ca19b8b2
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-03-10 15:55:30 +0100

    Change endpoint to /healthz (k8s standard) and move configuration to own class. Also use the Microsoft serializer to write directly to the response.

--- Source/Server/Web/HostBuilderExtensions.cs
+++ Source/Server/Web/HostBuilderExtensions.cs
@@ -1,22 +1,11 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
 using Dolittle.Runtime.Hosting;
-using Dolittle.Runtime.Server.HealthChecks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Polly;
 
 namespace Dolittle.Runtime.Server.Web;
 
@@ -39,19 +28,7 @@ public static class HostBuilderExtensions
                 });
                 webHost.Configure(app =>
                 {
-                    app.UseHealthChecks("/healthcheck",
-                        new HealthCheckOptions
-                        {
-                            ResponseWriter = async (context, report) =>
-                            {
-                                var result = JsonConvert.SerializeObject(
-                                    report,
-                                    Formatting.Indented,
-                                    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, Converters = new List<JsonConverter>{new StringEnumConverter()}});
-                                context.Response.ContentType = MediaTypeNames.Application.Json;
-                                await context.Response.WriteAsync(result);
-                            }
-                        });
+                    app.UseHealthChecks("/healthz");
                     app.UseRouting();
                     app.UseEndpoints(endpoints =>
                     {
