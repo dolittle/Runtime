38622c59d83283e28161084989356b73f0a89d4e
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-03-29 16:44:36 +0200

    Add a little more useful benchmarks

--- Benchmarks/Events.Store/CommitEvents.cs
+++ Benchmarks/Events.Store/CommitEvents.cs
@@ -8,7 +8,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Dolittle.Runtime.Artifacts;
-using Dolittle.Runtime.Domain.Tenancy;
 using Dolittle.Runtime.Events.Store;
 using Dolittle.Runtime.Events.Store.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +15,9 @@ using ExecutionContext = Dolittle.Runtime.Execution.ExecutionContext;
 
 namespace Benchmarks.Events.Store;
 
+/// <summary>
+/// Benchmarks for committing events using the <see cref="IEventStoreService"/>.
+/// </summary>
 public class CommitEvents : JobBase
 {
     IEventStoreService _eventStoreService;
@@ -34,18 +36,51 @@ public class CommitEvents : JobBase
             events.Add(new UncommittedEvent(
                 new EventSourceId("8453f4d4-861a-4042-a4e3-c1abf1c8eadd"),
                 new Artifact(new ArtifactId(Guid.Parse("752f88c9-70f0-4ffe-82b6-a69dcc96672e")), ArtifactGeneration.First),
-                Public,
+                false,
                 "{ \"hello\": \"world\" }"));
         }
         _eventsToCommit = new UncommittedEvents(events);
+
+        if (PreExistingEvents < 1)
+        {
+            return;
+        }
+        
+        var preExistingEvents = new List<UncommittedEvent>();
+        for (var n = 0; n < PreExistingEvents; n++)
+        {
+            preExistingEvents.Add(new UncommittedEvent(
+                new EventSourceId("8453f4d4-861a-4042-a4e3-c1abf1c8eadd"),
+                new Artifact(new ArtifactId(Guid.Parse("752f88c9-70f0-4ffe-82b6-a69dcc96672e")), ArtifactGeneration.First),
+                false,
+                "{ \"hello\": \"world\" }"));
+        }
+
+        var result = _eventStoreService.TryCommit(
+            new UncommittedEvents(preExistingEvents),
+            _executionContext,
+            CancellationToken.None).GetAwaiter().GetResult();
+        if (!result.Success)
+        {
+            throw result.Exception;
+        }
     }
     
-    [Params(false, true)]
-    public bool Public { get; set; }
+    /// <summary>
+    /// Gets the number of events that have been committed to the Event Store before the benchmarking commits.
+    /// </summary>
+    [Params(0, 100, 1000)]
+    public int PreExistingEvents { get; set; }
     
+    /// <summary>
+    /// Gets the number of events to be committed in the benchmarks.
+    /// </summary>
     [Params(1, 10, 50, 100)]
     public int EventsToCommit { get; set; }
 
+    /// <summary>
+    /// Commits the events one-by-one in a loop.
+    /// </summary>
     [Benchmark]
     public async Task CommitEventsInLoop()
     {
@@ -62,6 +97,9 @@ public class CommitEvents : JobBase
         }
     }
 
+    /// <summary>
+    /// Commits the events in a single batch.
+    /// </summary>
     [Benchmark]
     public async Task CommitEventsInBatch()
     {
