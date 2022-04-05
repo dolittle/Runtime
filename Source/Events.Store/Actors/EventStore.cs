bd14e5bde7f0a9a04ce899a30648e2305beb64f4
woksin <sindrewilting@gmail.com>
2022-04-05 22:05:23 +0200

    Complete the EventStore actor functionality wise and add docs

--- Source/Events.Store/Actors/EventStore.cs
+++ Source/Events.Store/Actors/EventStore.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Dolittle.Runtime.Actors;
 using Dolittle.Runtime.Events.Contracts;
 using Dolittle.Runtime.Events.Store.Persistence;
 using Dolittle.Runtime.Protobuf;
@@ -14,30 +15,44 @@ using Failure = Dolittle.Runtime.Protobuf.Failure;
 
 namespace Dolittle.Runtime.Events.Store.Actors;
 
+/// <summary>
+/// Represents the event store grain. 
+/// </summary>
+[TenantGrain(typeof(EventStoreActor), typeof(EventStoreClient))]
 public class EventStore : EventStoreBase
 {
-    TaskCompletionSource<Try> _nextBatchResult = new(TaskCreationOptions.RunContinuationsAsynchronously);
+    TaskCompletionSource<Try> _nextBatchResult;
 
     Task<Try> OnNextBatchCompleted => _nextBatchResult.Task;
-
     CommitBuilder CommitBuilder { get; set; }
 
     readonly IPersistCommits _commits;
+    readonly IFetchCommittedEvents _committedEvents;
 
     EventLogSequenceNumber _nextSequenceNumber;
     bool _readyToSend = true;
 
-    public EventStore(IContext context, IPersistCommits commits)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EventStore"/> class.
+    /// </summary>
+    /// <param name="context">The actor context.</param>
+    /// <param name="commits">The <see cref="IPersistCommits"/>.</param>
+    /// <param name="committedEvents">The <see cref="IFetchCommittedEvents"/> for getting the next event log sequence number.</param>
+    public EventStore(IContext context, IPersistCommits commits, IFetchCommittedEvents committedEvents)
         : base(context)
     {
         _commits = commits;
+        _committedEvents = committedEvents;
     }
 
+    /// <inheritdoc />
     public override async Task OnStarted()
     {
         _nextSequenceNumber = await GetNextEventLogSequenceNumber(Context.CancellationToken).ConfigureAwait(false);
+        ResetBatchBuilderState(_nextSequenceNumber);
     }
 
+    /// <inheritdoc />
     public override Task Commit(CommitEventsRequest request, Action<CommitEventsResponse> respond, Action<string> onError)
     {
         var tryAdd = CommitBuilder.TryAddEventsFrom(request);
@@ -76,6 +91,7 @@ public class EventStore : EventStoreBase
         return Task.CompletedTask;
     }
 
+    /// <inheritdoc />
     public override Task CommitForAggregate(CommitAggregateEventsRequest request, Action<CommitAggregateEventsResponse> respond, Action<string> onError)
     {
         var tryAdd = CommitBuilder.TryAddEventsFrom(request);
@@ -114,11 +130,13 @@ public class EventStore : EventStoreBase
         return Task.CompletedTask;
     }
 
+    /// <inheritdoc />
     public override Task<CommitEventsResponse> Commit(CommitEventsRequest request)
     {
         throw new NotImplementedException("Unused");
     }
 
+    /// <inheritdoc />
     public override Task<CommitAggregateEventsResponse> CommitForAggregate(CommitAggregateEventsRequest request)
     {
         throw new NotImplementedException("Unused");
@@ -166,15 +184,12 @@ public class EventStore : EventStoreBase
     void ResetBatchBuilderState(EventLogSequenceNumber newNextSequenceNumber)
     {
         CommitBuilder = new CommitBuilder(newNextSequenceNumber);
-        _nextBatchResult = new TaskCompletionSource<Try>();
+        _nextBatchResult = new TaskCompletionSource<Try>(TaskCreationOptions.RunContinuationsAsynchronously);
     }
 
     Task<EventLogSequenceNumber> GetNextEventLogSequenceNumber(CancellationToken cancellationToken)
-    {
-        // TODO: Get from event log
-        return Task.FromResult(EventLogSequenceNumber.Initial);
-    }
-    
+        => _committedEvents.FetchNextSequenceNumber(cancellationToken);
+
     static bool TryGetFailure(Task<Try> task, out Failure failure)
     {
         if (!task.IsCompletedSuccessfully)
