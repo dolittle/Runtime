9491c7835ac859485293e769bb52611e16c890dd
woksin <sindrewilting@gmail.com>
2022-04-25 14:45:28 +0200

    Add bootstrap project

---
+++ Source/Bootstrap/ICanPerformBoostrapProcedure.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Dolittle.Runtime.Domain.Tenancy;
+
+namespace Dolittle.Runtime.Server.Bootstrap;
+
+/// <summary>
+/// Defines a system that can perform a bootstrap procedure.
+/// </summary>
+public interface ICanPerformBoostrapProcedure
+{
+    /// <summary>
+    /// Performs a bootstrap procedure.
+    /// </summary>
+    /// <returns>The <see cref="Task"/> representing the asynchronous action.</returns>
+    public Task Perform();
+    
+    /// <summary>
+    /// Performs a bootstrap procedure for a specific tenant. 
+    /// </summary>
+    /// <returns>The <see cref="Task"/> representing the asynchronous action.</returns>
+    public Task PerformForTenant(TenantId tenant);
+}
