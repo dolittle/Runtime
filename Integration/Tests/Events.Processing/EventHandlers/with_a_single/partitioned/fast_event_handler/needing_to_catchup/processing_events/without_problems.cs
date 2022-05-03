ebe70287e9a7c9cb4fff3a409b5d5de51c3acf5d
woksin <sindrewilting@gmail.com>
2022-05-03 08:45:31 +0200

    WIP integration tests for event handlers

---
+++ Integration/Tests/Events.Processing/EventHandlers/with_a_single/partitioned/fast_event_handler/needing_to_catchup/processing_events/without_problems.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using Dolittle.Runtime.Events.Processing.EventHandlers;
+using Dolittle.Runtime.Events.Store;
+using Machine.Specifications;
+
+namespace Integration.Tests.Events.Processing.EventHandlers.with_a_single.partitioned.fast_event_handler.needing_to_catchup.processing_events;
+
+class without_problems : given.single_tenant_and_event_handlers
+{
+    static IEventHandler event_handler;
+
+    Establish context = () =>
+    {
+        commit_events_for_each_event_type(10, "some event source").GetAwaiter().GetResult();
+        with_event_handlers((true, number_of_event_types, ScopeId.Default, true));
+        event_handler = event_handlers_to_run.First();
+    };
+
+    Because of = () =>
+    {
+        stop_event_handlers_after(TimeSpan.FromSeconds(10));
+        run_event_handlers_until_completion_and_commit_events_after_starting_event_handlers().GetAwaiter().GetResult();
+    };
+
+    It should_have_persisted_stream_definition = () => get_stream_definition_for(event_handler).Success.ShouldBeTrue();
+    It should_have_persisted_a_partitioned_stream_definition = () => get_stream_definition_for(event_handler).ShouldBeTrue();
+}
---
+++ Integration/Tests/Events.Processing/given/a_clean_event_store.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Runtime.Domain.Tenancy;
+using Dolittle.Runtime.Events.Processing.Streams;
+using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Events.Store.MongoDB.Aggregates;
+using Dolittle.Runtime.Events.Store.MongoDB.Events;
+using Dolittle.Runtime.Events.Store.MongoDB.Streams;
+using Dolittle.Runtime.Events.Store.Streams;
+using Machine.Specifications;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using Event = Dolittle.Runtime.Events.Store.MongoDB.Events.Event;
+
+namespace Integration.Tests.Events.Processing.given;
+
+class a_clean_event_store : a_runtime_with_a_single_tenant
+{
+    protected static readonly FilterDefinition<Event> all_events_filter = Builders<Event>.Filter.Empty;
+    protected static IEventStore event_store;
+    protected static IEventContentConverter event_content_converter;
+    protected static IStreams streams;
+    protected static IResilientStreamProcessorStateRepository stream_processor_states;
+    protected static IStreamDefinitionRepository stream_definition_repository;
+
+
+    Establish context = () =>
+    {
+        event_store = runtime.Host.Services.GetRequiredService<Func<TenantId, IEventStore>>()(execution_context.Tenant);
+        event_content_converter = runtime.Host.Services.GetRequiredService<IEventContentConverter>();
+        streams = runtime.Host.Services.GetRequiredService<Func<TenantId, IStreams>>()(execution_context.Tenant);
+        stream_processor_states = runtime.Host.Services.GetRequiredService<Func<TenantId, IResilientStreamProcessorStateRepository>>()(tenant);
+        stream_definition_repository = runtime.Host.Services.GetRequiredService<Func<TenantId, IStreamDefinitionRepository>>()(tenant);
+    };
+}
