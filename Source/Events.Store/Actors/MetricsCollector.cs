f00be5590def0bada5c199e8445f81404d54d806
woksin <sindrewilting@gmail.com>
2022-09-12 13:08:25 +0200

    Implement metrics

--- Source/Events.Store/Actors/MetricsCollector.cs
+++ Source/Events.Store/Actors/MetricsCollector.cs
@@ -13,41 +13,79 @@ namespace Dolittle.Runtime.Events.Store.Actors;
 [Metrics, Singleton]
 public class MetricsCollector : IMetricsCollector
 {
-    readonly Counter _totalGet;
+    readonly Counter _totalCommitsReceived;
+    readonly Counter _totalCommitsForAggregateReceived;
+    readonly Counter _totalAggregateRootVersionCacheInconsistencies;
+    readonly Counter _totalBatchesSuccessfullyPersisted;
+    readonly Counter _totalBatchedEventsSuccessfullyPersisted;
+    readonly Counter _totalBatchesSent;
+    readonly Counter _totalAggregateRootVersionCacheInconsistenciesResolved;
+    readonly Counter _totalBatchesFailedPersisting;
 
     public MetricsCollector(IMetricFactory metricFactory)
     {
-        _totalGet = metricFactory.CreateCounter(
-            "dolittle_shared_runtime_projections_store_get_total",
-            "ProjectionStore total number of Get requests");
+        _totalCommitsReceived = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_commits_received_total",
+            "EventStore total number of non-aggregate commits received");
         
+        _totalCommitsForAggregateReceived = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_commits_for_aggregate_received_total",
+            "EventStore total number of commits for aggregate received");
+        
+        _totalAggregateRootVersionCacheInconsistencies = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_aggregate_root_version_cache_inconsistencies_total",
+            "EventStore total number of aggregate root version cache inconsistencies occurred");
+        
+        _totalBatchesSuccessfullyPersisted = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_batches_successfully_persisted_total",
+            "EventStore total number of batches that has been successfully persisted");
+        
+        _totalBatchedEventsSuccessfullyPersisted = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_batched_events_successfully_persisted_total",
+            "EventStore total number of batched events that has been successfully persisted");
+         
+        _totalBatchesSent = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_batches_sent_total",
+            "EventStore total number of batches that has been sent to the event store");
+        
+        _totalAggregateRootVersionCacheInconsistenciesResolved = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_aggregate_root_version_cache_inconsistencies_resolved_total",
+            "EventStore total number of aggregate root version cache inconsistencies that has been resolved");
+
+        _totalBatchesFailedPersisting = metricFactory.CreateCounter(
+            "dolittle_shared_runtime_events_store_batches_failed_persisting_total",
+            "EventStore total number of batches that failed to be persisted");
     }
 
     /// <inheritdoc />
-    public void IncrementTotalGet() 
-        => _totalGet.Inc();
-
     public void IncrementTotalCommitsReceived()
-        => throw new System.NotImplementedException();
+        => _totalCommitsReceived.Inc();
 
+    /// <inheritdoc />
     public void IncrementTotalCommitsForAggregateReceived()
-        => throw new System.NotImplementedException();
+        => _totalCommitsForAggregateReceived.Inc();
 
+    /// <inheritdoc />
     public void IncrementTotalAggregateRootVersionCacheInconsistencies()
-        => throw new System.NotImplementedException();
+        => _totalAggregateRootVersionCacheInconsistencies.Inc();
 
+    /// <inheritdoc />
     public void IncrementTotalBatchesSuccessfullyPersisted()
-        => throw new System.NotImplementedException();
+        => _totalBatchesSuccessfullyPersisted.Inc();
 
+    /// <inheritdoc />
     public void IncrementTotalBatchedEventsSuccessfullyPersisted(int allEventsCount)
-        => throw new System.NotImplementedException();
+        => _totalBatchedEventsSuccessfullyPersisted.Inc(allEventsCount);
 
+    /// <inheritdoc />
     public void IncrementTotalBatchesSent()
-        => throw new System.NotImplementedException();
+        => _totalBatchesSent.Inc();
 
+    /// <inheritdoc />
     public void IncrementTotalAggregateRootVersionCacheInconsistenciesResolved()
-        => throw new System.NotImplementedException();
+        => _totalAggregateRootVersionCacheInconsistenciesResolved.Inc();
 
+    /// <inheritdoc />
     public void IncrementTotalBatchesFailedPersisting()
-        => throw new System.NotImplementedException();
+        => _totalBatchesFailedPersisting.Inc();
 }
