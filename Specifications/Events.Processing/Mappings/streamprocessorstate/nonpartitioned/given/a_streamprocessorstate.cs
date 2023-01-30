0e96ae12bc0f51f50c8aced39e6ce004364324e1
Magne Helleborg <magne.helleborg@gmail.com>
2023-01-30 13:25:02 +0100

    Mappings tests

---
+++ Specifications/Events.Processing/Mappings/streamprocessorstate/nonpartitioned/given/a_streamprocessorstate.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Runtime.Events.Processing.Streams;
+using Dolittle.Runtime.Events.Store.Streams;
+
+
+namespace Dolittle.Runtime.Events.Processing.Mappings.given;
+
+public class a_streamprocessorstate
+{
+    protected static readonly StreamProcessorState stream_processor_state = new(new StreamPosition(10), DateTimeOffset.Now);
+}
