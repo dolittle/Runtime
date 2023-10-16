92e530d1d7135b426ed1905488d5734831a7ae4d
Magne Helleborg <magne.helleborg@gmail.com>
2023-10-16 08:24:25 +0200

    Cleanup

--- Specifications/Events.Processing/Mappings/streamprocessorstate/partitioned/failing/mapping_is_correct.cs
+++ Specifications/Events.Processing/Mappings/streamprocessorstate/partitioned/failing/mapping_is_correct.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using Dolittle.Runtime.Events.Processing.Streams.Partitioned;
 using Dolittle.Runtime.Events.Store.Actors;
 using FluentAssertions;
