86299b67a00258520d5c06769f358a95bc8a3a10
Magne Helleborg <magne.helleborg@gmail.com>
2023-11-01 11:37:13 +0100

    Reduce channel size for EventLogStream, to have more control over memory usage

--- Source/Events/Store/Actors/EventLogStream.cs
+++ Source/Events/Store/Actors/EventLogStream.cs
@@ -26,7 +26,7 @@ namespace Dolittle.Runtime.Events.Store;
 [Singleton, PerTenant]
 public class EventLogStream : IEventLogStream
 {
-    const int ChannelSize = 3;
+    const int ChannelSize = 2;
 
     readonly ActorSystem _actorSystem;
     readonly ICreateProps _createProps;
