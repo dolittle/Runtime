00a6a8f1ae0e9ad2917b22d206121c1c752e2044
Magne Helleborg <magne.helleborg@gmail.com>
2024-05-31 13:40:07 +0200

    OTEL metrics for event processing

--- Source/Events/Processing/EventHandlers/MetricsCollector.cs
+++ Source/Events/Processing/EventHandlers/MetricsCollector.cs
@@ -2,7 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using Dolittle.Runtime.DependencyInversion.Lifecycle;
+using Dolittle.Runtime.Diagnostics.OpenTelemetry.Metrics;
 using Dolittle.Runtime.Domain.Tenancy;
 using Dolittle.Runtime.Events.Store;
 using Dolittle.Runtime.Events.Store.Streams;
@@ -25,7 +28,15 @@ public class MetricsCollector : IMetricsCollector
     readonly Counter _customerEventProcessingFailuresTotal;
     readonly Histogram _customerEventProcessingTime;
 
-    /// <summary>
+    // OpenTelemetry
+    readonly Counter<long> _customerRegistrationsTotalOtel;
+    readonly Counter<long> _systemRegistrationsTotalOtel;
+    readonly Counter<long> _customerFailedRegistrationsTotalOtel;
+    readonly Counter<long> _systemFailedRegistrationsTotalOtel;
+    readonly Counter<long> _customerEventProcessingFailuresTotalOtel;
+    readonly Histogram<TimeSpan> _customerEventProcessingTimeOtel;
+
+   /// <summary>
     /// Creates a new instance of the <see cref="MetricsCollector"/> class.
     /// </summary>
     /// <param name="metricFactory">The metric factory to use to create metrics.</param>
@@ -61,43 +72,111 @@ public class MetricsCollector : IMetricsCollector
             "The processing time of events processed by event handlers",
                 new[] { "scope", "eventHandlerId", "eventHandlerAlias", "tenantId", "eventTypeId", "eventTypeAlias" },
             new[] { 0.001, 0.01, 0.1, 1, 10 });
+    
+        // OpenTelemetry metrics initialization
+        _customerRegistrationsTotalOtel = RuntimeMetrics.Meter.CreateCounter<long>(
+            "dolittle_customer_runtime_eventhandlers_registrations_total",
+            "count",
+            "Total number of event handler registrations",
+            new Dictionary<string, object?>
+            {
+                { "eventHandlerId", "Event handler id" },
+                { "eventHandlerAlias", "Event handler alias" }
+            });
+
+        _systemRegistrationsTotalOtel = RuntimeMetrics.Meter.CreateCounter<long>(
+            "dolittle_system_runtime_eventhandlers_registrations_total",
+            "count",
+            "Total number of event handler registrations");
+
+        _customerFailedRegistrationsTotalOtel = RuntimeMetrics.Meter.CreateCounter<long>(
+            "dolittle_customer_runtime_eventhandlers_failed_registrations_total",
+            "count",
+            "Total number of failed event handler registrations");
+
+        _systemFailedRegistrationsTotalOtel = RuntimeMetrics.Meter.CreateCounter<long>(
+            "dolittle_system_runtime_eventhandlers_failed_registrations_total",
+            "count",
+            "Total number of failed event handler registrations");
+
+        _customerEventProcessingFailuresTotalOtel = RuntimeMetrics.Meter.CreateCounter<long>(
+            "dolittle_customer_runtime_eventhandlers_event_processing_failures_total",
+            "count",
+            "Total number of failed event processing attempts by an event handler");
+
+        _customerEventProcessingTimeOtel = RuntimeMetrics.Meter.CreateHistogram<TimeSpan>(
+            "dolittle_customer_runtime_eventhandlers_event_processing_time_seconds",
+            "count",
+            "The processing time of events processed by event handlers");
     }
 
-    /// <inheritdoc />
     public void IncrementRegistrationsTotal(EventHandlerInfo info)
     {
-        _customerRegistrationsTotal.WithLabels(info.Id.EventHandler.ToString(), info.HasAlias ? info.Alias : string.Empty).Inc();
+        string handlerAlias = info.HasAlias ? info.Alias : string.Empty;
+        var handlerId = info.Id.EventHandler.ToString();
+        _customerRegistrationsTotal.WithLabels(handlerId, handlerAlias).Inc();
         _systemRegistrationsTotal.Inc();
+        _customerRegistrationsTotalOtel.Add(1, new KeyValuePair<string, object?>("eventHandlerId", handlerId), new KeyValuePair<string, object?>("eventHandlerAlias", handlerAlias));
+        _systemRegistrationsTotalOtel.Add(1);
     }
 
-    /// <inheritdoc />
-    public void IncrementFailedRegistrationsTotal(EventHandlerInfo info) 
+    public void IncrementFailedRegistrationsTotal(EventHandlerInfo info)
     {
-        _customerFailedRegistrationsTotal.WithLabels(info.Id.EventHandler.ToString(), info.HasAlias ? info.Alias : string.Empty).Inc();
+        string handlerAlias = info.HasAlias ? info.Alias : string.Empty;
+        var handlerId = info.Id.EventHandler.ToString();
+        _customerFailedRegistrationsTotal.WithLabels(handlerId, handlerAlias).Inc();
         _systemFailedRegistrationsTotal.Inc();
+        _customerFailedRegistrationsTotalOtel.Add(1, new KeyValuePair<string, object?>("eventHandlerId", handlerId), new KeyValuePair<string, object?>("eventHandlerAlias", handlerAlias));
+        _systemFailedRegistrationsTotalOtel.Add(1);
     }
 
-    /// <inheritdoc />
     public void IncrementEventsProcessedTotal(EventHandlerInfo info, TenantId tenant, StreamEvent @event, TimeSpan processingTime)
     {
+        var scopeId = info.Id.Scope.ToString();
+        var handlerId = info.Id.EventHandler.ToString();
+        string handlerAlias = info.HasAlias ? info.Alias : string.Empty;
+        var tenantId = tenant.ToString();
+        var eventTypeId = @event.Event.Type.Id.ToString();
+        var eventTypeAlias = _eventTypes.GetEventTypeAliasOrEmptyString(@event.Event.Type);
         _customerEventProcessingTime.WithLabels(
-                info.Id.Scope.ToString(),
-                info.Id.EventHandler.ToString(),
-                info.HasAlias ? info.Alias : string.Empty,
-                tenant.ToString(),
-                @event.Event.Type.Id.ToString(),
-                _eventTypes.GetEventTypeAliasOrEmptyString(@event.Event.Type))
+                scopeId,
+                handlerId,
+                handlerAlias,
+                tenantId,
+                eventTypeId,
+                eventTypeAlias)
             .Observe(processingTime.TotalSeconds);
+        _customerEventProcessingTimeOtel.Record(processingTime,
+            new KeyValuePair<string, object?>("scope", scopeId),
+            new KeyValuePair<string, object?>("eventHandlerId", handlerId),
+            new KeyValuePair<string, object?>("eventHandlerAlias", handlerAlias),
+            new KeyValuePair<string, object?>("tenantId", tenantId),
+            new KeyValuePair<string, object?>("eventTypeId", eventTypeId),
+            new KeyValuePair<string, object?>("eventTypeAlias", eventTypeAlias));
     }
 
-    /// <inheritdoc />
     public void IncrementEventProcessingFailuresTotal(EventHandlerInfo info, TenantId tenant, StreamEvent @event)
-        => _customerEventProcessingFailuresTotal.WithLabels(
-                info.Id.Scope.ToString(),
-                info.Id.EventHandler.ToString(),
-                info.HasAlias ? info.Alias : string.Empty,
-                tenant.ToString(),
-                @event.Event.Type.Id.ToString(),
-                _eventTypes.GetEventTypeAliasOrEmptyString(@event.Event.Type))
+    {
+        var scopeId = info.Id.Scope.ToString();
+        var eventHandlerId = info.Id.EventHandler.ToString();
+        string handlerAlias = info.HasAlias ? info.Alias : string.Empty;
+        var tenantId = tenant.ToString();
+        var eventTypeId = @event.Event.Type.Id.ToString();
+        var eventHandlerAlias = _eventTypes.GetEventTypeAliasOrEmptyString(@event.Event.Type);
+        _customerEventProcessingFailuresTotal.WithLabels(
+                scopeId,
+                eventHandlerId,
+                handlerAlias,
+                tenantId,
+                eventTypeId,
+                eventHandlerAlias)
             .Inc();
+        _customerEventProcessingFailuresTotalOtel.Add(1,
+            new KeyValuePair<string, object?>("scope", scopeId),
+            new KeyValuePair<string, object?>("eventHandlerId", eventHandlerId),
+            new KeyValuePair<string, object?>("eventHandlerAlias", handlerAlias),
+            new KeyValuePair<string, object?>("tenantId", tenantId),
+            new KeyValuePair<string, object?>("eventTypeId", eventTypeId),
+            new KeyValuePair<string, object?>("eventTypeAlias", eventHandlerAlias));
+    }
 }
